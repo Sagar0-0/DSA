3ab997f1858fc4693308d4b8d89e7bf255dbe42e
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-02-18 14:20:33 +0530

    Update Rearrange Geek and his Classmates.java

--- Rearrange Geek and his Classmates.java
+++ Rearrange Geek and his Classmates.java
@@ -9,3 +9,24 @@ class Solution
             a[i]/=n;
    }
 } 
+
+
+===================================================================
+    
+    class Solution 
+{ 
+    void prank(long[] a, int n)  
+    { 
+       // code here
+       long[] b=new long[n];
+       for(int i=0;i<n;i++){
+           long temp=a[i];
+           b[i]= a[(int)temp];
+          }
+       for(int i=0;i<n;i++)
+       {
+           a[i]=b[i];
+       }
+      
+   }
+} 
