b23cd5aa92770c98554602303d8a70e8725efb04
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-02-22 11:08:43 +0530

    Update Excel Sheet Column Number.java

--- Excel Sheet Column Number.java
+++ Excel Sheet Column Number.java
@@ -7,3 +7,21 @@ class Solution {
         return ans;
     }
 }
+
+//===========================================================
+
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        if(columnTitle == null || columnTitle.length() == 0){
+            return 0;
+        }
+        int multipier = 0, result = 0;
+        for(int i = columnTitle.length() -1 ; i >= 0 ; i-- ){
+            int val = columnTitle.charAt(i) - 'A'+1;
+            result = (int) (val * Math.pow(26,multipier)) + result;
+            multipier += 1;
+            //System.out.println(val+"  result : "+result+"  multipier :"+multipier);
+        }
+        return result;
+    }
+}
