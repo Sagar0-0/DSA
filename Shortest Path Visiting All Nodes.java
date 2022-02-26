fdb0eb180eccf615b0c3cee8ea814011c361a403
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-02-26 21:30:26 +0530

    Create Shortest Path Visiting All Nodes.java

---
+++ Shortest Path Visiting All Nodes.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int shortestPathLength(int[][] graph) {
+        if (graph.length == 1) {
+            return 0;
+        }
+        
+        int n = graph.length;
+        int endingMask = (1 << n) - 1;
+        boolean[][] seen = new boolean[n][endingMask];
+        ArrayList<int[]> queue = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            queue.add(new int[] {i, 1 << i});
+            seen[i][1 << i] = true;
+        }
+        
+        int steps = 0;
+        while (!queue.isEmpty()) {
+            ArrayList<int[]> nextQueue = new ArrayList<>();
+            for (int i = 0; i < queue.size(); i++) {
+                int[] currentPair = queue.get(i);
+                int node = currentPair[0];
+                int mask = currentPair[1];
+                for (int neighbor : graph[node]) {
+                    int nextMask = mask | (1 << neighbor);
+                    if (nextMask == endingMask) {
+                        return 1 + steps;
+                    }
+                    
+                    if (!seen[neighbor][nextMask]) {
+                        seen[neighbor][nextMask] = true;
+                        nextQueue.add(new int[] {neighbor, nextMask});
+                    }
+                }
+            }
+            steps++;
+            queue = nextQueue;
+        }
+        
+        return -1;
+    }
+}
