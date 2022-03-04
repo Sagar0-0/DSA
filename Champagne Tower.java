3e224e00ddfebbab319f140261e72190a30f50b8
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-03-04 12:14:57 +0530

    Update Champagne Tower.java

--- Champagne Tower.java
+++ Champagne Tower.java
@@ -18,3 +18,27 @@ class Solution {
         return Math.min(1, champagneTower[query_row][query_glass]);
     }
 } 
+
+//======================================================================================================
+
+class Solution {
+    public double champagneTower(int poured, int queryRow, int queryGlass) {
+	    if (poured == 0)
+		    return 0;
+	    var prevRow = new ArrayList<>(List.of((double) poured));
+
+	    while (queryRow-- > 0) {
+		    var currentRow = new ArrayList<Double>();
+		    var champagneInEnds = Math.max(0, (prevRow.get(0) - 1) / 2);  // min champagne can be 0
+		    currentRow.add(champagneInEnds); // first glass
+
+		    for (var i = 1; i < prevRow.size(); i++)
+			    currentRow.add(Math.max(0, (prevRow.get(i - 1) - 1) / 2) + // flow from top-left glass
+						   Math.max(0, (prevRow.get(i) - 1) / 2));     // flow from top-right glass
+
+		    currentRow.add(champagneInEnds); // last glass
+		    prevRow = currentRow;
+	    }
+	    return Math.min(1, prevRow.get(queryGlass)); // max champagne can be 1
+    }
+} 
