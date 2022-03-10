c8fe54dc2ed23c7e0a51930726e0a858fdc0185a
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-03-10 16:48:16 +0530

    Create Longest Common Subsequence.java

---
+++ Longest Common Subsequence.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int solve(String A, String B) {
+        int n=A.length();
+        int m=B.length();
+        int[][]dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }else if(A.charAt(i-1)==B.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
