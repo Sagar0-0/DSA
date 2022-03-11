fbdee45cebc12cf5c68f9b9f94d5f917d3577e36
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-03-11 13:56:08 +0530

    Update Rotate List.java

--- Rotate List.java
+++ Rotate List.java
@@ -30,3 +30,34 @@ class Solution {
         
     }
 }
+
+//=================================================================================
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null)
+            return head;
+        int size = getListSize(head);
+        k %= size;
+        if (k == 0)
+            return head;
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i = 0; i < size-k; i++) {
+            slow = fast;
+            fast = fast.next;
+        }
+        slow.next = null;
+        ListNode oldHead = head;
+        head = fast;
+        while (fast.next != null)
+            fast = fast.next;
+        fast.next = oldHead;
+        return head;
+    }
+    
+    public int getListSize(ListNode head) {
+        if (head == null)
+            return 0;
+        return 1 + getListSize(head.next);
+    }
+}
