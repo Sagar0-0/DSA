7196e7bcfcaac99755a0c0f80abfda5b71d5d4ba
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-03-14 13:46:08 +0530

    Update Biconnected Graph.java

--- Biconnected Graph.java
+++ Biconnected Graph.java
@@ -1,15 +1,15 @@
 class Solution {
-    static int biGraph(int[] arr, int n, int e) {
-        if (n == 2 && e == 1) {
-            return 1;
+   static int biGraph(int[] arr, int n, int e) {
+       // code here
+        if(n<=2) return 1;
+        int p= 2* e;
+        int freq[]= new int[n];
+        for(int i=0;i<p;i+=2){
+             freq[arr[i]]++;
+             freq[arr[i+1]]++;
         }
-        final List<Integer> allEdges = new ArrayList<>(e*2);
-        Arrays.stream(arr).forEach(x -> allEdges.add(x));
-        for(int i=0; i<n; i++) {
-            if (Collections.frequency(allEdges, i) <= 1) {
-                return 0;
-            }
-        }
+        for(int num:freq)
+            if(num<=1)return 0;
         return 1;
-    }
+   }
 };
