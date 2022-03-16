a797481be4da11e7ff5263817716b0474318fdc6
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-03-16 11:49:49 +0530

    Update Validate Stack Sequences.java

--- Validate Stack Sequences.java
+++ Validate Stack Sequences.java
@@ -25,3 +25,27 @@ class Solution {
         return true;
     }
 }
+//=============================================
+class Solution {
+    public boolean validateStackSequences(int[] arr, int[] pop) {
+       
+        int n=arr.length;
+         if(n==0)
+            return true;
+        int j=0;
+        int i=-1;
+        for(int e:arr)
+        {
+                arr[++i]=e;
+                while(i>=0&&arr[i]==pop[j])
+                {
+                    i--;
+                    j++;
+                }
+        }
+        if(i==-1&&j==n)
+            return true;
+        return false;
+            
+    }
+}
