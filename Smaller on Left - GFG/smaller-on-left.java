e315d0c4b3cecc02e5af5da530fe3a2f3eb1a065
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-03-22 19:29:18 +0530

    Added solution - LeetHub

---
+++ Smaller on Left - GFG/smaller-on-left.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    int sizeOfArray = Integer.parseInt(br.readLine());
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Complete obj = new Complete();
+		    arr = obj.Smallestonleft(arr, sizeOfArray);
+		    for(int i=0; i< sizeOfArray;i++){
+		        System.out.print(arr[i] + " ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Complete{
+    public static int[] Smallestonleft (int arr[], int n) {
+        TreeSet<Integer> set = new TreeSet<>();
+        int ans[] = new int[n];
+        
+        for(int i=0; i<n; i++){
+            Integer num = set.lower(arr[i]);
+            if(num!=null){
+                ans[i] = num;
+            }else{
+                ans[i] = -1;
+            }
+            set.add(arr[i]);
+        }
+        return ans;
+    }
+}
+
