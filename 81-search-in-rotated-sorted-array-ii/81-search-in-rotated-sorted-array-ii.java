e89909c9c65effd608095b13bfc8179e8e58c557
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-03-29 21:07:54 +0530

    Time: 1 ms (81.35%), Space: 44.1 MB (25.00%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,45 +1,28 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int len = nums.length;
-        int l = 0;
-        int h = len - 1;
-
-        int mid;
-        while(l <= h) {
-            mid = (l + (h-l) / 2);
-            if (nums[mid] == target) {
+         int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
                 return true;
-            }
-            if(!isHelpful(nums,l,mid)){
-                l++;
-                continue;
-            }
-            
-            boolean midInFirst = inFirst(nums[l],nums[mid]);
-            boolean tarInFirst = inFirst(nums[l],target);
-            
-            if(midInFirst^tarInFirst){//One in first and one in second
-                if(midInFirst){
-                    l = mid + 1;
+            }else if(nums[left]==nums[mid] && nums[right]==nums[mid]){
+                left++;
+                right--;
+            }else if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && nums[mid]>target){
+                    right=mid-1;
                 }else{
-                    h = mid - 1;
+                    left=mid+1;
                 }
-            }else {//both are in the first or both are in the second
-                if (nums[mid] > target) {
-                    h = mid - 1;
-                } else {
-                    l = mid + 1;
+            }else{
+                if(nums[mid]<target && nums[right]>=target){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
                 }
             }
         }
         return false;
-    }
-    
-    boolean isHelpful(int[] nums, int start, int midIdx){
-        return nums[start] != nums[midIdx];
-    }
-    
-    boolean inFirst(int startVal, int i){
-        return startVal <= i;
     }
 }
