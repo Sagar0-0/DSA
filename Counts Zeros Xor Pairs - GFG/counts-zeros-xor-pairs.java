4b0f915242bb4937163a1192a5a35b5eba4256ca
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-03-30 14:05:30 +0530

    Added solution - LeetHub

---
+++ Counts Zeros Xor Pairs - GFG/counts-zeros-xor-pairs.java
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    
+		    Complete obj = new Complete();
+		    long res = obj.calculate(arr, sizeOfArray);
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static long calculate (int arr[], int n) {
+        //Complete the function
+        Arrays.sort(arr);
+        int ans=0;
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(arr[i]==arr[i-1]){
+                count++;
+            }else{
+                ans+=(count*(count-1)/2);
+                count=1;
+            }
+        }
+        ans+=(count*(count-1)/2);
+        return ans;
+    }
+    
+    
+}
+
+
