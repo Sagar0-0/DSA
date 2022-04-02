ed1b2d6f998eba54ca2a140403c54e26699f580b
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-04-02 13:35:58 +0530

    Added solution - LeetHub

---
+++ Move all zeros to the front of the linked list - GFG/move-all-zeros-to-the-front-of-the-linked-list.java
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+class Zeroes{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            Node head=null;
+            while(n-->0){
+                int a=sc.nextInt();
+                if(head==null){
+                    head=new Node(a);
+                }
+                else{
+                    Node temp=new Node(a);
+                    temp.next=head;
+                    head=temp;
+                }
+            }
+            GfG g=new GfG();
+            head = g.moveZeroes(head);
+            while(head!=null){
+                System.out.print(head.data+" ");
+                head=head.next;
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+*/
+
+class GfG{
+    public Node moveZeroes(Node head){
+        Node temp=head;
+        while(temp.next!=null){
+            if(temp.next.data==0){
+                Node zero=temp.next;
+                temp.next=temp.next.next;
+                zero.next=head;
+                head=zero;
+            }else{
+                temp=temp.next;
+            }
+        }
+        return head;
+    }
+}
