8de09990f963b11fd171603564f3e27d3409df64
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-04-13 12:14:49 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (85.14%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -2,23 +2,20 @@ class Solution {
     public int[][] generateMatrix(int n) {
         int[][] result = new int[n][n];
         int cnt = 1;
-        for (int layer = 0; layer < (n + 1) / 2; layer++) {
-            // direction 1 - traverse from left to right
-            for (int ptr = layer; ptr < n - layer; ptr++) {
-                result[layer][ptr] = cnt++;
-            }
-            // direction 2 - traverse from top to bottom
-            for (int ptr = layer + 1; ptr < n - layer; ptr++) {
-                result[ptr][n - layer - 1] = cnt++;
-            }
-            // direction 3 - traverse from right to left
-            for (int ptr = layer + 1; ptr < n - layer; ptr++) {
-                result[n - layer - 1][n - ptr - 1] = cnt++;
-            }
-            // direction 4 - traverse from bottom to top
-            for (int ptr = layer + 1; ptr < n - layer - 1; ptr++) {
-                result[n - ptr - 1][layer] = cnt++;
-            }
+        int dir[][] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int d = 0;
+        int row = 0;
+        int col = 0;
+        while (cnt <= n * n) {
+            result[row][col] = cnt++;
+            int r = Math.floorMod(row + dir[d][0], n);
+            int c = Math.floorMod(col + dir[d][1], n);
+
+            // change direction if next cell is non zero
+            if (result[r][c] != 0) d = (d + 1) % 4;
+
+            row += dir[d][0];
+            col += dir[d][1];
         }
         return result;
     }
