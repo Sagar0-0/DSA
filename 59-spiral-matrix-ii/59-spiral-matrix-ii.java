c655ebc6b566c42b490230d1871472741ad14caa
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-04-13 12:16:00 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (87.79%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int[][] result = new int[n][n];
-        int cnt = 1;
-        int dir[][] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int d = 0;
-        int row = 0;
-        int col = 0;
-        while (cnt <= n * n) {
-            result[row][col] = cnt++;
-            int r = Math.floorMod(row + dir[d][0], n);
-            int c = Math.floorMod(col + dir[d][1], n);
-
-            // change direction if next cell is non zero
-            if (result[r][c] != 0) d = (d + 1) % 4;
-
-            row += dir[d][0];
-            col += dir[d][1];
+        
+        int ans[][] = new int[n][n];
+        fillMatrix(ans, 0 , 0, 1,'r');
+        return ans;
+    }
+    
+    public void fillMatrix(int [][]ans, int i, int j, int cur, char dir){
+        if(i < 0 || j < 0 || i >=ans.length ||  j >= ans.length) return;
+        
+        if(ans[i][j] != 0) return;
+        
+        ans[i][j] = cur;
+        
+        if(dir == 'u'){
+            fillMatrix(ans, i-1,j,cur+1, 'u');    
         }
-        return result;
+        
+        fillMatrix(ans, i,j+1,cur+1, 'r');
+        fillMatrix(ans, i+1,j,cur+1, 'd');
+        fillMatrix(ans, i,j-1,cur+1, 'l');
+        fillMatrix(ans, i-1,j,cur+1, 'u');
+       
     }
 }
