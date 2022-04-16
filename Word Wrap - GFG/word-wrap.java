d9772988cbcad61def540bce45783afca765faa0
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-04-17 00:33:52 +0530

    Added solution - LeetHub

--- Word Wrap - GFG/word-wrap.java
+++ Word Wrap - GFG/word-wrap.java
@@ -24,54 +24,47 @@ class GFG
 // } Driver Code Ends
 
 
-class Solution
-{
-    private int getCost(int[] nums, int from, int to, int k){
-       if(to == nums.length-1) return 0;
-       
-       int used = 0;
-       
-       for(int i=from; i<=to; i++){
-           if(used > 0) used++;
-           used += nums[i];    
-       }
-       
-       return (k-used)*(k-used);
-   }
-   
-   public int solveWordWrapRec (int[] nums, int k, int idx, Map<Integer, Integer> cache){
-       if(idx >= (nums.length-1)){
+class Solution{
+    public int solveWordWrap (int[] nums, int k){
+        return solveWordWrapRec(nums, k, 0, new HashMap<>());
+    }
+    
+    public int solveWordWrapRec (int[] nums, int k, int idx, Map<Integer, Integer> cache){
+        if(idx >= (nums.length-1)){
            return 0;
-       }
+        }
        
-       Integer value = cache.get(idx);
-       if(value != null) return value;
+        Integer value = cache.get(idx);
+        if(value != null) return value;
        
-       int cost = Integer.MAX_VALUE;
-       int used = 0;
+        int cost = Integer.MAX_VALUE;
+        int used = 0;
        
-       for(int i=idx; i<nums.length; i++){
-           if(used+nums[i] > k) break;
-           
-           if(i+1 < nums.length){
-               cost = Math.min(cost, solveWordWrapRec(nums, k, i+1, cache) + getCost(nums, idx, i, k));
-           }else{
-               cost = Math.min(cost, getCost(nums, idx, i, k));
-           }
-           
-           used += nums[i] + 1;
-       }
-       
-       value = cost;
-       cache.put(idx, value);
-       
-       return value;
-   }
-
- 
-
-    public int solveWordWrap (int[] nums, int k)
-   {
-       return solveWordWrapRec(nums, k, 0, new HashMap<>());
-   }
+        for(int i=idx; i<nums.length; i++){
+            if(used+nums[i] > k) break;
+            
+            if(i+1 < nums.length){
+                cost = Math.min(cost, solveWordWrapRec(nums, k, i+1, cache) + getCost(nums, idx, i, k));
+            }else{
+                cost = Math.min(cost, getCost(nums, idx, i, k));
+            }
+            used += nums[i] + 1;
+        }
+        value = cost;
+        cache.put(idx, value);
+        
+        return value;
+    }
+    
+    private int getCost(int[] nums, int from, int to, int k){
+        if(to == nums.length-1) return 0;
+        int used = 0;
+        
+        for(int i=from; i<=to; i++){
+            if(used > 0) used++;
+            used += nums[i];    
+        }
+        
+        return (k-used)*(k-used);
+    }
 }
