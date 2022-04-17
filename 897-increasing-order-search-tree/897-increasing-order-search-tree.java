be84d198f439f2040a3248c23d3947af8e69140f
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-04-17 14:04:34 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (78.88%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.java
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.java
@@ -1,19 +1,23 @@
 class Solution {
     public TreeNode increasingBST(TreeNode root){
-        List<Integer> list=new ArrayList<>();
+        List<TreeNode> list=new ArrayList<>();
         inOrder(root,list);
         TreeNode newHead=new TreeNode(-1);
         TreeNode temp=newHead;
         for(int i=0;i<list.size();i++){
-            temp.right=new TreeNode(list.get(i));
+            temp.right=list.get(i);
             temp=temp.right;
         }
         return newHead.right;
     }
-    public void inOrder(TreeNode root,List<Integer> list){
+    public void inOrder(TreeNode root,List<TreeNode> list){
         if(root==null)return;
-        inOrder(root.left,list);
-        list.add(root.val);
-        inOrder(root.right,list);
+        TreeNode lefty=root.left;
+        root.left=null;
+        TreeNode righty=root.right;
+        root.right=null;
+        inOrder(lefty,list);
+        list.add(root);
+        inOrder(righty,list);
     }
 }
