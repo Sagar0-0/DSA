7046b2f5d5674d62bcf41aaef6d23ead51af4b7e
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-04-17 17:09:05 +0530

    Added solution - LeetHub

--- Closest Palindrome - GFG/closest-palindrome.java
+++ Closest Palindrome - GFG/closest-palindrome.java
@@ -24,90 +24,86 @@ class GFG
 
 
 //User function Template for Java
-
 class Solution
 {
     public long closestPalindrome(long num)
     {
-        // Code here
-       if(num<=9) return num;
-	        
-	        if(check(num)) return num-1;
-	        
-	        String s = Long.toString(num);
-	        if(isPall(s)){
-	            
-	            return num;
-	        }
-	        StringBuilder sb = new StringBuilder();
-	        
-	        boolean flag = false;
-	        if(s.length()%2==1) flag = true;
-	        
-	        for(int i=0;i<s.length()/2;i++){
-	            sb.append(s.charAt(i));
-	        }
-	        if(flag){
-	            sb.append(s.charAt(s.length()/2));
-	        }
-//	        System.out.println(sb);
-	        long n = Long.parseLong(sb.toString());
-            
-            long fir = n+1;
-            long sec = n-1;
-//            System.out.println(fir);
-            StringBuilder sb1 = new StringBuilder(Long.toString(fir));
-            
-            int i=sb1.length()-1;
-            if(flag) {
-            	i--;
-            }
-            while(i>=0) {
-            	sb1.append(sb1.charAt(i--));
-            }
-            StringBuilder sb2 = new StringBuilder(Long.toString(sec));
-            
-             i=sb2.length()-1;
-             if(flag) {
-	            	i--;
-	            }
-            while(i>=0) {
-            	sb2.append(sb2.charAt(i--));
-            }
-//            System.out.println(sb1+" "+sb2);
-            long a = Long.parseLong(sb1.toString());
-            long b = Long.parseLong(sb2.toString());
-	        
-	        i=sb.length()-1;
-	        if(flag) i--;
-	        while(i>=0) {
-            	sb.append(sb.charAt(i--));
-            }
-	        
-	        long c =  Long.parseLong(sb.toString());
-	        long diff = Math.abs(c-num);
-	        
-	        long min = Math.min(a-num, Math.min(diff, num-b));
-	       // System.out.println(c);
-	        if(min==num-b && min==diff) return Math.min(b,c);
-	        else if(min==a-num && min==diff) return Math.min(a,c);
-	        else if(min==num-b) return b;
-	        else if(min==a-num) return a;
-	        
-	        else return c;
+        String number = String.valueOf(num);
         
-    }
-    private static boolean check(long num){
-        while(num>1 && num%10==0){
-            num/=10;
-            
+        if(isPalindrome(number))
+        {
+            return num;
         }
-        return num==1;
-    }
-    private static boolean isPall(String s){
-        StringBuilder sb = new StringBuilder(s);
-        sb = sb.reverse();
         
-        return s.equals(sb.toString());
+        if(number.charAt(0)=='1' && Long.parseLong(number.substring(1))==0)
+        {
+            return num-1;
+        }
+        
+        int n = number.length();
+        
+        boolean hasEvenDigits = n%2==0;
+        
+        
+        String palRootStr = hasEvenDigits ? number.substring(0, n/2) : number.substring(0, n/2+1);
+        
+        long palRoot = Long.parseLong(palRootStr);
+        
+        //Equal
+        
+        long equal = toPal("" + palRoot, hasEvenDigits);
+        long diffEqual = Math.abs(num-equal);
+        
+        //Bigger
+        
+        long bigger = toPal("" + (palRoot+1), hasEvenDigits);
+        long diffBigger = Math.abs(num-bigger);
+        
+        //Smaller
+        
+        long smaller = toPal("" + (palRoot-1), hasEvenDigits);
+        long diffSmaller = Math.abs(num-smaller);
+        
+        
+        long closest = diffBigger<diffSmaller ? bigger : smaller;
+        
+        long minDiff = Math.min(diffBigger, diffSmaller);
+        
+        if(diffEqual==minDiff)
+        {
+            closest = Math.min(equal, closest);
+        }
+        else if(diffEqual<minDiff)
+        {
+            closest = equal;
+        }
+        
+        
+        return closest;
+        
+        
     }
+    
+    private long toPal(String num, boolean isEven)
+    {
+        StringBuilder reverseNum = new StringBuilder(num).reverse();
+        
+        String palDigits = isEven ? num + reverseNum : num + (reverseNum.deleteCharAt(0)).toString();
+        
+        return Long.parseLong(palDigits);
+    }
+    
+    
+    private boolean isPalindrome(String num)
+    {
+        int l=0, r=num.length()-1;
+        
+        while(l<r)
+        {
+            if(num.charAt(l++)!=num.charAt(r--)) return false;
+        }
+        
+        return true;
+    }
+    
 }
