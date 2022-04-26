cd71ca6a560dc4b34af0923b1c6b258c64332b38
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-04-26 19:42:17 +0530

    Added solution - LeetHub

--- Eulerian Path in an Undirected Graph - GFG/eulerian-path-in-an-undirected-graph.java
+++ Eulerian Path in an Undirected Graph - GFG/eulerian-path-in-an-undirected-graph.java
@@ -33,10 +33,12 @@ class Solution{
         int count = 0;
         for(int ar[]: arr){
             int sum = 0;
-            for(int i: ar) sum += i;
-            if(sum%2!=0) count++;
-            if(count>2) return 0;
+            for(int i: ar){
+                sum+=i;
+            }
+            if(sum%2==0)count++;
+            if(count==(n-2))return 1;
         }
-        return 1-count%2;
+        return 0;
     }
 }
