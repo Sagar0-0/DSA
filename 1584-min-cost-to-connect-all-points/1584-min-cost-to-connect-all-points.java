cfc3d45da32ad9d1f39770ce9770a132376849bf
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-04-26 20:28:42 +0530

    Time: 20 ms (99.89%), Space: 42.1 MB (98.44%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,42 +1,45 @@
 class Solution {
+    protected int getManDist(int[] a, int[] b) {
+        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+    } 
+    
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
-        
-        // Min-heap to store minimum weight edge at top.
-        PriorityQueue<Pair<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> (a.getKey() - b.getKey()));;
-        
-        // Track nodes which are included in MST.
-        boolean[] inMST = new boolean[n];
-        
-        heap.add(new Pair(0, 0));
-        int mstCost = 0;
-        int edgesUsed = 0;
-        
-        while (edgesUsed < n) {
-            Pair<Integer, Integer> topElement = heap.poll();
-            
-            int weight = topElement.getKey();
-            int currNode = topElement.getValue();
-            
-            // If node was already included in MST we will discard this edge.
-            if (inMST[currNode]) {
-                continue;
-            }
-            
-            inMST[currNode] = true;
-            mstCost += weight;
-            edgesUsed++;
-            
-            for (int nextNode = 0; nextNode < n; ++nextNode) {
-                // If next node is not in MST, then edge from curr node
-                // to next node can be pushed in the priority queue.
-                if (!inMST[nextNode]) {
-                    int nextWeight = Math.abs(points[currNode][0] - points[nextNode][0]) + Math.abs(points[currNode][1] - points[nextNode][1]);
-        
-                    heap.add(new Pair(nextWeight, nextNode));
-                }
-            }
+        if (points.length < 2) {
+            return 0;
         }
-        return mstCost;
+        
+        int n = points.length;
+        boolean[] inMST = new boolean[n];
+        int[] curMinDistToEachNode = new int[n];
+        int cost = 0;
+        int edges = 0;
+        int cur = 0;
+        
+        while (edges < n - 1) {
+            int minIndex = 0;
+            int minCost = Integer.MAX_VALUE;
+            inMST[cur] = true;
+            
+            for (int i = 0; i < n; ++i) {
+                if (!inMST[i]) {
+                    int dist = getManDist(points[cur], points[i]);
+                    if (cur == 0 || curMinDistToEachNode[i] > dist) {
+                        curMinDistToEachNode[i] = dist;
+                    }
+                    
+                    if (curMinDistToEachNode[i] < minCost) {
+                        minCost = curMinDistToEachNode[i];
+                        minIndex = i;
+                    }
+                }               
+            }
+            
+            cost += minCost;
+            cur = minIndex;
+            edges++;
+        }
+        
+        
+        return cost;
     }
 }
