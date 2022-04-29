aac93c82866d2c3c1b1726e5c193a1f728280d76
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-04-29 19:39:47 +0530

    Added solution - LeetHub

--- Find length of Loop - GFG/find-length-of-loop.java
+++ Find length of Loop - GFG/find-length-of-loop.java
@@ -79,7 +79,7 @@ class Solution
         Node fast=head;
         Node slow=head;
         boolean has=false;
-        while(slow != null && slow.next != null && fast != null && fast.next != null){
+        while(fast != null && fast.next != null){
             fast=fast.next.next;
             slow=slow.next;
             if(fast==slow){
