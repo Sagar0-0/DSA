7b2a8d4c4a3c714286be3d3d4a9035775ede3b0c
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-01 18:12:14 +0530

    Time: 1 ms (87.56%), Space: 41.4 MB (73.74%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,27 +1,51 @@
 class Solution {
-    public boolean backspaceCompare(String S, String T) {
-        int i = S.length() - 1, j = T.length() - 1;
-        int skipS = 0, skipT = 0;
-
-        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
-            while (i >= 0) { // Find position of next possible char in build(S)
-                if (S.charAt(i) == '#') {skipS++; i--;}
-                else if (skipS > 0) {skipS--; i--;}
-                else break;
-            }
-            while (j >= 0) { // Find position of next possible char in build(T)
-                if (T.charAt(j) == '#') {skipT++; j--;}
-                else if (skipT > 0) {skipT--; j--;}
-                else break;
-            }
-            // If two actual characters are different
-            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j))
+    public boolean backspaceCompare(String s, String t) {
+        int sPointer = s.length() - 1;
+        int tPointer = t.length() - 1;
+        
+        while (sPointer >= 0 || tPointer >= 0) {
+            sPointer = getNextCharacterPosition(s, sPointer); // can be negative, empty string;
+            tPointer = getNextCharacterPosition(t, tPointer); // can be negative, empty string;
+            
+            if ((sPointer < 0 && tPointer >= 0) ||
+                (tPointer < 0 && sPointer >= 0) ||
+                (sPointer >= 0 && tPointer >= 0) && (s.charAt(sPointer) != t.charAt(tPointer))
+               ) {
                 return false;
-            // If expecting to compare char vs nothing
-            if ((i >= 0) != (j >= 0))
-                return false;
-            i--; j--;
+            }
+            
+            sPointer--;
+            tPointer--;
         }
+        
         return true;
+    }
+
+    public int getNextCharacterPosition(String s, int start) {
+        if (start < 0) {
+            return start;
+        }
+        
+        char c = s.charAt(start);
+        int count = c == '#' ? 2 : 0;
+        
+        while (count > 0) {
+            start--;
+            
+            if (start < 0) {
+                start -= count;
+                break;
+            }
+            
+            c = s.charAt(start);
+            
+            if (c == '#') {
+                count++;
+            } else {
+                count--;
+            }
+        }
+               
+        return start;
     }
 }
