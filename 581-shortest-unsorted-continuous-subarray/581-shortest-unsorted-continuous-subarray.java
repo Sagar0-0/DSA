5fd703127041c46ad11c640c3398ee556bfdd0d4
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-03 22:05:18 +0530

    Time: 3 ms (49.15%), Space: 53.9 MB (58.62%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,18 +1,29 @@
 public class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        Stack < Integer > stack = new Stack < Integer > ();
-        int l = nums.length, r = 0;
-        for (int i = 0; i < nums.length; i++) {
-            while (!stack.isEmpty() && nums[stack.peek()] > nums[i])
-                l = Math.min(l, stack.pop());
-            stack.push(i);
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        boolean flag = false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1])
+                flag = true;
+            if (flag)
+                min = Math.min(min, nums[i]);
         }
-        stack.clear();
-        for (int i = nums.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums[stack.peek()] < nums[i])
-                r = Math.max(r, stack.pop());
-            stack.push(i);
+        flag = false;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] > nums[i + 1])
+                flag = true;
+            if (flag)
+                max = Math.max(max, nums[i]);
         }
-        return r - l > 0 ? r - l + 1 : 0;
+        int l, r;
+        for (l = 0; l < nums.length; l++) {
+            if (min < nums[l])
+                break;
+        }
+        for (r = nums.length - 1; r >= 0; r--) {
+            if (max > nums[r])
+                break;
+        }
+        return r - l < 0 ? 0 : r - l + 1;
     }
 }
