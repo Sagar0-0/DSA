f6a0a6c7613ef5c654237bce6b4f7a591ca7b14a
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-06 16:17:21 +0530

    Time: 6 ms (98.01%), Space: 48.9 MB (61.80%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,52 +1,22 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-        StringBuilder sb = new StringBuilder();
-        Stack<Pair> st = new Stack();
-        int len = s.length();
-        char c = ' ';
-        int cur = 0;
-        for(int i = 0; i < len; i++)
-        {
-            c = s.charAt(i);
-            if(!st.empty())
-            {
-                Pair p = st.peek();
-                if(c == p.letter)
-                {
-                    int j = p.count + 1;
-                    st.pop();
-                    st.push(new Pair(c, j));
+        int w = 0;
+        int n = s.length();
+        int []count = new int[n];
+        char[] ar = s.toCharArray();
+        for(int i=0;i<n;i++){
+            ar[w]=ar[i];          
+            if(w==0||ar[w-1]!=ar[w]){
+                count[w]=1;
+            }else{
+                count[w]=count[w-1]+1;
+                if(count[w]==k){
+                    w=w-k;
                 }
-                else
-                    st.push(new Pair(c, 1));
             }
-            else
-                st.push(new Pair(c, 1));
-            
-            if(st.peek().count == k)
-                st.pop();
+            w++;
         }
         
-        for(Pair p: st)
-        {
-            int i = p.count;
-            while(i > 0)
-            {
-                sb.append(p.letter);
-                i--;
-            }
-        }
-        return sb.toString();
-    }
-    
-    class Pair {
-        char letter;
-        int count;
-        
-        public Pair(char c, int count)
-        {
-            letter = c;
-            this.count = count;
-        }
+        return new String(ar, 0, w);
     }
 }
