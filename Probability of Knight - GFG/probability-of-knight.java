cb302163e6c0bcca8af8b504757f44271991d4b4
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-09 14:05:48 +0530

    Added solution - LeetHub

--- Probability of Knight - GFG/probability-of-knight.java
+++ Probability of Knight - GFG/probability-of-knight.java
@@ -40,89 +40,81 @@ class Solution
           return false;
       }
     }
-   public double findProb(int N, int start_x, int start_y, int step)
-   {
-       // Code here
-       // findprob - custom function code
-       
-       double curr[][] = new double[N][N];
-       double next[][] = new double[N][N];
+    public double findProb(int N, int start_x, int start_y, int step){
+        double curr[][] = new double[N][N];
+        double next[][] = new double[N][N];
  
-   curr[start_x][start_y] = 1;
+        curr[start_x][start_y] = 1;
  
-   for (int m = 1; m<=step; m++){
+        for (int m = 1; m<=step; m++){
+            for (int i = 0; i<N; i++){
+                for (int j = 0; j<N; j++){
+                    if(curr[i][j] != 0){
+                        int ni = 0;
+                        int nj = 0;
+                        ni = i-2;
+                        nj = j+1;
+                        if(isValid(ni, nj, N)){
+                            next[ni][nj] += curr[i][j]/8.0;
+                        }
+                    
+                        ni = i-1;
+                        nj = j+2;
+                        if(isValid(ni, nj, N)){
+                            next[ni][nj] += curr[i][j]/8.0;
+                        }
+                     
+                         ni = i+1;
+                         nj = j+2;
+                         if(isValid(ni, nj, N)){
+                             next[ni][nj] += curr[i][j]/8.0;
+                         }
+                         
+                         ni = i+2;
+                         nj = j+1;
+                         if(isValid(ni, nj, N)){
+                             next[ni][nj] += curr[i][j]/8.0;
+                         }
+                         
+                         ni = i+2;
+                         nj = j-1;
+                         if(isValid(ni, nj, N)){
+                             next[ni][nj] += curr[i][j]/8.0;
+                         }
+                         
+                         ni = i+1;
+                         nj = j-2;
+                         if(isValid(ni, nj, N)){
+                             next[ni][nj] += curr[i][j]/8.0;
+                         }
+                         
+                         ni = i-1;
+                         nj = j-2;
+                         if(isValid(ni, nj, N)){
+                             next[ni][nj] += curr[i][j]/8.0;
+                         }
+                         
+                        ni = i-2;
+                        nj = j-1;
+                        if(isValid(ni, nj, N)){
+                            next[ni][nj] += curr[i][j]/8.0;
+                        }
+                     
+                    }
+                }
+            }
+           curr = next;
+           next = new double[N][N];
+         
+         
+        }
      
+       double sum = 0;
        for (int i = 0; i<N; i++){
            for (int j = 0; j<N; j++){
-               if(curr[i][j] != 0){
-                 
-                 int ni = 0;
-                 int nj = 0;
-                 
-                 ni = i-2;
-                 nj = j+1;
-                 if(isValid(ni, nj, N)){
-                     next[ni][nj] += curr[i][j]/8.0;
-                 }
-                 
-                 ni = i-1;
-                 nj = j+2;
-                 if(isValid(ni, nj, N)){
-                     next[ni][nj] += curr[i][j]/8.0;
-                 }
-                 
-                 ni = i+1;
-                 nj = j+2;
-                 if(isValid(ni, nj, N)){
-                     next[ni][nj] += curr[i][j]/8.0;
-                 }
-                 
-                 ni = i+2;
-                 nj = j+1;
-                 if(isValid(ni, nj, N)){
-                     next[ni][nj] += curr[i][j]/8.0;
-                 }
-                 
-                 ni = i+2;
-                 nj = j-1;
-                 if(isValid(ni, nj, N)){
-                     next[ni][nj] += curr[i][j]/8.0;
-                 }
-                 
-                 ni = i+1;
-                 nj = j-2;
-                 if(isValid(ni, nj, N)){
-                     next[ni][nj] += curr[i][j]/8.0;
-                 }
-                 
-                 ni = i-1;
-                 nj = j-2;
-                 if(isValid(ni, nj, N)){
-                     next[ni][nj] += curr[i][j]/8.0;
-                 }
-                 
-                 ni = i-2;
-                 nj = j-1;
-                 if(isValid(ni, nj, N)){
-                     next[ni][nj] += curr[i][j]/8.0;
-               }
-                 
-               }
+               sum += curr[i][j];
            }
        }
-     
-       curr = next;
-       next = new double[N][N];
-     
-     
-   }
- 
-   double sum = 0;
-   for (int i = 0; i<N; i++){
-       for (int j = 0; j<N; j++){
-           sum += curr[i][j];
-       }
-   }
        
        return sum;
    }
