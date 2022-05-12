7d195c665100c259aa01fe427bc35496d667b245
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-12 11:48:49 +0530

    Time: 3 ms (83.29%), Space: 47.8 MB (62.27%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,32 +1,32 @@
 class Solution {
-    List<List<Integer>> ans;
-    Set<List<Integer>> set;
+    
+    List<List<Integer>> lists;
+    boolean[] added;
+    
     public List<List<Integer>> permuteUnique(int[] nums) {
-        set=new HashSet<>();
-        ans=new ArrayList<>();
-        int n=nums.length;
-        boolean[] vis=new boolean[n];
-        dfs(nums,vis,n,new ArrayList<>());
-        return ans;
+        lists = new ArrayList<>();
+        Arrays.sort(nums);
+        added = new boolean[nums.length];
+        permuteUnique(nums, new ArrayList<>());
+        return lists;
     }
-    public void dfs(int[]nums,boolean[]vis,int n,List<Integer> curr){
-        if(n==0){
-            if(!set.contains(curr)){
-                set.add(curr);
-                List<Integer> newCurr=new ArrayList<>();
-                newCurr.addAll(curr);
-                ans.add(newCurr);
-            }
+    
+    private void permuteUnique(int[] nums, List<Integer> list) {
+        if(list.size() == nums.length) {
+            lists.add(new ArrayList(list));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!vis[i]){
-                curr.add(nums[i]);
-                vis[i]=true;
-                dfs(nums,vis,n-1,curr);
-                curr.remove(curr.size()-1);
-                vis[i]=false;
-            }
+        for(int i = 0; i < nums.length; i++) {
+            if(added[i])
+                continue;
+            // avoid permute duplicates
+            if(i > 0 && nums[i] == nums[i - 1] && !added[i - 1])
+                continue;
+            list.add(nums[i]);
+            added[i] = true;
+            permuteUnique(nums, list);
+            list.remove(list.size() - 1);
+            added[i] = false;
         }
     }
 }
