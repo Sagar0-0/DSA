f5ac96a1162d88789197df3f005d52d244b70d49
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-13 12:14:53 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (34.72%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -1,42 +1,30 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
     public Node connect(Node root) {
-        if(root==null)return null;
-        Queue<Node> q=new LinkedList<>();
-        q.add(root);
-        while(q.size()>0){
-            q.add(null);
-            int sz=q.size();
-            for(int i=0;i<sz-1;i++){
-                Node n=q.remove();
-                n.next=q.peek();
-                if(n.left!=null)q.add(n.left);
-                if(n.right!=null)q.add(n.right);
-            }
-            q.remove();
+        if (root == null || (root.left == null && root.right == null) ) {
+            return root;
         }
+        
+        // connect left child to right child
+        if (root.left != null && root.right != null) {
+            root.left.next = root.right;
+        }
+        
+        // connect right child (or left if right is abasent) to child of next nodes.
+        Node lNode = (root.right == null) ? root.left : root.right;
+        
+        Node next = root.next;
+        while (next != null && next.left == null && next.right == null) {
+            next = next.next;
+        }
+
+        if (next != null) {
+            lNode.next = (next.left != null) ? next.left : next.right;
+        }
+        
+        // process right child first
+        connect(root.right);
+        connect(root.left);
+        
         return root;
     }
 }
