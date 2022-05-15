daca600b68f4b1ee9f5a75a1b1595484dbd8364b
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-15 19:00:07 +0530

    Time: 1 ms (100.00%), Space: 44.6 MB (88.87%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -1,39 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int res;
-    int depth;
+    int deepestLevel = 0;
+    int sum;
+    
     public int deepestLeavesSum(TreeNode root) {
-        depth=findDepth(root);
-        preOrder(root,1);
-        return res;
+        deepestLeavesSum(root, 0);
+        return sum;
     }
-    public void preOrder(TreeNode root,int lvl){
-        if(root==null)return;
-        if(lvl==depth){
-            res+=root.val;
+    
+    public void deepestLeavesSum(TreeNode root,int level) {
+        if (root == null) {
             return;
         }
-        preOrder(root.left,lvl+1);
-        preOrder(root.right,lvl+1);
-    }
-    public int findDepth(TreeNode root){
-        if(root==null)return 0;
-        int lefty=findDepth(root.left);
-        int righty=findDepth(root.right);
-        return Math.max(lefty,righty)+1;
+        
+        if (root.left == null && root.right == null && level == deepestLevel) {
+            sum = sum + root.val;
+        }
+        
+        if (root.left == null && root.right == null && level > deepestLevel) {
+            deepestLevel = level;
+            sum = root.val;
+        }
+        
+        deepestLeavesSum(root.left, level + 1);
+        deepestLeavesSum(root.right, level + 1);
     }
 }
