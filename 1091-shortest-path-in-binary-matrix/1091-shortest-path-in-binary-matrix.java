57faad5c9d1208a2a34f695c8a8c1593fe9194ce
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-16 12:38:21 +0530

    Time: 8 ms (99.93%), Space: 43.5 MB (89.09%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,68 +1,58 @@
 class Solution {
-    class Pair{
-        int x;
-        int y;
-        Pair(int a,int b){
-            x=a;
-            y=b;
-        }
-    }
-    public int shortestPathBinaryMatrix(int[][] grid) {
-        Queue<Pair> q=new LinkedList<>();
+   public int shortestPathBinaryMatrix(int[][] grid) {
         int n=grid.length;
-        int ans=1;
-        boolean[][]vis=new boolean[n][n];
-        if(valid(grid,0,0,vis))
-            q.add(new Pair(0,0));
-        vis[0][0]=true;
-        while(q.size()>0 && !vis[n-1][n-1]){
-            ans++;
-            int sz=q.size();
-            for(int a=0;a<sz;a++){
-                Pair pair=q.remove();
-                int i=pair.x;
-                int j=pair.y;
-                if(valid(grid,i+1,j+1,vis)){
-                    q.add(new Pair(i+1,j+1));
-                    vis[i+1][j+1]=true;
-                }
-                if(valid(grid,i-1,j-1,vis)){
-                    q.add(new Pair(i-1,j-1));
-                    vis[i-1][j-1]=true;
-                }
-                if(valid(grid,i+1,j,vis)){
-                    q.add(new Pair(i+1,j));
-                    vis[i+1][j]=true;
-                }
-                if(valid(grid,i-1,j,vis)){
-                    q.add(new Pair(i-1,j));
-                    vis[i-1][j]=true;
-                }
-                if(valid(grid,i+1,j-1,vis)){
-                    q.add(new Pair(i+1,j-1));
-                    vis[i+1][j-1]=true;
-                }
-                if(valid(grid,i,j+1,vis)){
-                    q.add(new Pair(i,j+1));
-                    vis[i][j+1]=true;
-                }
-                if(valid(grid,i,j-1,vis)){
-                    q.add(new Pair(i,j-1));
-                    vis[i][j-1]=true;
-                }
-                if(valid(grid,i-1,j+1,vis)){
-                    q.add(new Pair(i-1,j+1));
-                    vis[i-1][j+1]=true;
-                }
+        if(grid[0][0]==1||grid[n-1][n-1]==1)return -1;
+        if(n==1&&grid[0][0]==0)return 1;
+        grid[0][0]=1;
+        List<int[]> pos=new ArrayList<>();
+        pos.add(new int[]{0,0});
+        check(grid,pos);
+        if(grid[n-1][n-1]==0)return -1;
+        return grid[n-1][n-1];
+        
+    }
+    public void check(int[][] grid,List<int[]> pos){
+        int n=grid.length;
+        List<int[]> newpos=new ArrayList<>();
+        for(int[] p:pos){
+            int i=p[0];
+            int j=p[1];
+            int curdis=grid[i][j]+1;
+            if(i>0&&j>0&&grid[i-1][j-1]==0){
+                grid[i-1][j-1]=curdis;
+                newpos.add(new int[]{i-1,j-1});
+            }
+            if(i<n-1&&j>0&&grid[i+1][j-1]==0){
+                grid[i+1][j-1]=curdis;
+                newpos.add(new int[]{i+1,j-1});
+            }
+            if(i>0&&j<n-1&&grid[i-1][j+1]==0){
+                grid[i-1][j+1]=curdis;
+                newpos.add(new int[]{i-1,j+1});
+            }
+            if(i<n-1&&j<n-1&&grid[i+1][j+1]==0){
+                grid[i+1][j+1]=curdis;
+                newpos.add(new int[]{i+1,j+1});
+            }
+            if(j>0&&grid[i][j-1]==0){
+                grid[i][j-1]=curdis;
+                newpos.add(new int[]{i,j-1});
+            }
+            if(j<n-1&&grid[i][j+1]==0){
+                grid[i][j+1]=curdis;
+                newpos.add(new int[]{i,j+1});
+            }
+            if(i<n-1&&grid[i+1][j]==0){
+                grid[i+1][j]=curdis;
+                newpos.add(new int[]{i+1,j});
+            }
+            if(i>0&&grid[i-1][j]==0){
+                grid[i-1][j]=curdis;
+                newpos.add(new int[]{i-1,j});
             }
         }
-        if(vis[n-1][n-1]==false)return -1;
-        return ans;
-    }
-    public boolean valid(int[][]grid,int i,int j,boolean[][]vis){
-        int n=grid.length;
-        if(i<0 || j<0 || i==n || j==n)return false;
-        if(vis[i][j] || grid[i][j]==1)return false;
-        return true;
+        if(!newpos.isEmpty())check(grid,newpos);
+        
+        
     }
 }
