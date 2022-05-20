35d4ce50c22cfea3079230ee65b0c90cfe0eb5bb
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-20 15:36:16 +0530

    Time: 1 ms (52.25%), Space: 41.9 MB (67.23%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,42 +1,38 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-
-        int R = obstacleGrid.length;
-        int C = obstacleGrid[0].length;
-
-        // If the starting cell has an obstacle, then simply return as there would be
-        // no paths to the destination.
-        if (obstacleGrid[0][0] == 1) {
-            return 0;
+    public int uniquePathsWithObstacles(int[][] a) {
+        int result = 0;
+        int m = a.length;
+        int n = a[0].length;
+        int[] row = new int[n];
+        int prevCol;
+        row[0] = 1 - a[0][0];
+        for (int i = 1; i < n; ++i) {
+            if (a[0][i] == 1 || a[0][i-1] == 1) {
+                row[i] = 0;
+                continue;
+            }
+            row[i] = row[i-1];
         }
-
-        // Number of ways of reaching the starting cell = 1.
-        obstacleGrid[0][0] = 1;
-
-        // Filling the values for the first column
-        for (int i = 1; i < R; i++) {
-            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
-        }
-
-        // Filling the values for the first row
-        for (int i = 1; i < C; i++) {
-            obstacleGrid[0][i] = (obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1) ? 1 : 0;
-        }
-
-        // Starting from cell(1,1) fill up the values
-        // No. of ways of reaching cell[i][j] = cell[i - 1][j] + cell[i][j - 1]
-        // i.e. From above and left.
-        for (int i = 1; i < R; i++) {
-            for (int j = 1; j < C; j++) {
-                if (obstacleGrid[i][j] == 0) {
-                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
-                } else {
-                    obstacleGrid[i][j] = 0;
+        
+        for (int i = 1; i < m; ++i) {
+            if (a[i-1][0] == 1 || a[i][0] == 1) {
+                row[0] = 0;
+            }
+            for (int j = 1; j < n; ++j) {
+                if (a[i][j] == 1) {
+                    row[j] = 0;
+                    continue;
                 }
+                int tmp = 0;
+                if (a[i-1][j] != 1) {
+                    tmp += row[j];
+                }
+                if (a[i][j-1] != 1) {
+                    tmp += row[j-1];
+                }
+                row[j] = tmp;
             }
         }
-
-        // Return value stored in rightmost bottommost cell. That is the destination.
-        return obstacleGrid[R - 1][C - 1];
+        return row[n-1];
     }
 }
