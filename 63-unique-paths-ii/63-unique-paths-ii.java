fc71b74d6adcd229d97dad14c093d6af6603756b
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-20 10:50:27 +0530

    Time: 1 ms (52.25%), Space: 41.8 MB (69.26%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -4,12 +4,16 @@ class Solution {
         int R = obstacleGrid.length;
         int C = obstacleGrid[0].length;
 
+        // If the starting cell has an obstacle, then simply return as there would be
+        // no paths to the destination.
         if (obstacleGrid[0][0] == 1) {
             return 0;
         }
 
+        // Number of ways of reaching the starting cell = 1.
         obstacleGrid[0][0] = 1;
 
+        // Filling the values for the first column
         for (int i = 1; i < R; i++) {
             obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
         }
@@ -19,6 +23,9 @@ class Solution {
             obstacleGrid[0][i] = (obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1) ? 1 : 0;
         }
 
+        // Starting from cell(1,1) fill up the values
+        // No. of ways of reaching cell[i][j] = cell[i - 1][j] + cell[i][j - 1]
+        // i.e. From above and left.
         for (int i = 1; i < R; i++) {
             for (int j = 1; j < C; j++) {
                 if (obstacleGrid[i][j] == 0) {
@@ -28,6 +35,8 @@ class Solution {
                 }
             }
         }
+
+        // Return value stored in rightmost bottommost cell. That is the destination.
         return obstacleGrid[R - 1][C - 1];
     }
 }
