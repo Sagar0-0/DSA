f3dd950876c2be6ab440df5db6adb8cc52ba1497
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-21 13:19:23 +0530

    Time: 14 ms (90.78%), Space: 41.9 MB (90.07%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,22 +1,13 @@
 class Solution {
-public int coinChange(int[] coins, int amount) {
-
-    if (coins.length== 0 || amount== 0) {
-      return 0;
-    }
-
-    int[] table= new int[amount+1];
-    Arrays.fill(table, Integer.MAX_VALUE);
-    table[0]=0;
-
-    for (int coin : coins) {
-        for (int j = coin; j <= amount; j++) {
-            int index = j-coin;
-            if(table[index] != Integer.MAX_VALUE){
-              table[j] = Math.min(table[j], 1+table[index]);
+    public int coinChange(int[] coins, int amount) {
+        int dp[]=new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int coin:coins){
+            for(int i=coin;i<=amount;i++){//<=dp.length
+                dp[i]=Math.min(dp[i],dp[i-coin]+1);
             }
         }
+        return dp[amount]<=amount? dp[amount] : -1;
     }
-    return (table[amount] == Integer.MAX_VALUE)?-1:table[amount];
-  }
 }
