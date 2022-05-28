475a1243447b7470e5a8888f752a27ddd2aba3be
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-05-28 19:13:24 +0530

    Update Combination Sum.java

--- Combination Sum.java
+++ Combination Sum.java
@@ -1,22 +1,30 @@
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        backtrack(res, candidates, target, new ArrayList<>(), 0, 0);
-        return res;
+public class Solution {
+    ArrayList<ArrayList<Integer>> ans;
+    ArrayList<Integer> arr;
+    int target;
+    public ArrayList<ArrayList<Integer>> combinationSum(ArrayList<Integer> A, int B){
+        ans=new ArrayList<>();
+        target=B;
+        Collections.sort(A);
+        arr=A;
+        dfs(0,0,new ArrayList<>());
+        return ans;
     }
-    
-    private void backtrack(List<List<Integer>> res, int[] candidates, int target, List<Integer> potential, int currSum, int startIndex) {
-        if (currSum == target) {
-            res.add(new ArrayList<>(potential));
-            return;
-        }
-        for (int i = startIndex; i < candidates.length; i++) {
-            int num = candidates[i];
-            if (currSum + num <= target) {
-                potential.add(num);
-                backtrack(res, candidates, target, potential, currSum + num, i);
-                potential.remove(potential.size() - 1);
+    public void dfs(int sum,int index,ArrayList<Integer> curr){
+        if(sum>target)return;
+        if(sum==target){
+            if(!ans.contains(curr)){
+                ans.add(new ArrayList<>(curr));
             }
+        }
+        for(int i=index;i<arr.size();i++){
+            int ele=arr.get(i);
+            if(i>index && ele==arr.get(i-1))continue;
+            curr.add(ele);
+            sum+=ele;
+            dfs(sum,i,curr);
+            curr.remove(curr.size()-1);
+            sum-=ele;
         }
     }
 }
