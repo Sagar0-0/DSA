eace6cbc2ce735ce38f61e2427286ffb24534ef2
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-06-09 10:01:37 +0530

    Time: 1 ms (99.22%), Space: 50.6 MB (13.15%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,19 +1,39 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[]ans=new int[2];
-        int i=0;
-        int j=nums.length-1;
-        while(i<j){
-            if(nums[i]+nums[j]==target){
-                ans[0]=i+1;
-                ans[1]=j+1;
-                return ans;
-            }else if(nums[i]+nums[j]<target){
-                i++;
-            }else{
-                j--;
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        boolean targetLocated = false;
+        while (left < right && !targetLocated) {
+            int mid = left + (right - left) / 2;
+            int midVal = numbers[mid];
+            int curr = numbers[left] + numbers[right];
+            if(curr == target) {
+                targetLocated = true;
+            } else if (curr < target) {
+                if (midVal + numbers[right] < target) {
+                    left = mid + 1;
+                } else {
+                    left++;
+                }
+                
+            } else {
+                
+                if (midVal + numbers[left] > target) {
+                    right = mid - 1;
+                } else {
+                    right--;
+                }
+                
+                
             }
+            
         }
-        return ans;
+        
+        int[] vals = new int[2];
+        vals[0] = left + 1;
+        vals[1] = right + 1;
+        
+        return vals;
+        
     }
 }
