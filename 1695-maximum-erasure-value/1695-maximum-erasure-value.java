f961bb62943cc4faee04507887b40c74b16a68f2
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-06-12 10:11:20 +0530

    Time: 99 ms (51.10%), Space: 121 MB (32.46%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -5,32 +5,27 @@ class Solution {
         int i=0;
         int j=1;
         int ans=0;
+        int sum=nums[i];
         Set<Integer> set=new HashSet<>();
         set.add(nums[i]);
         while(j<n){
             int ele=nums[j];
             if(!set.contains(ele)){
                 set.add(ele);
+                sum+=ele;
                 j++;
             }else{
                 while(set.contains(ele)){
                     set.remove(nums[i]);
+                    sum-=nums[i];
                     i++;
                 }
                 set.add(ele);
+                sum+=ele;
                 j++;
             }
-            ans=Math.max(ans,sum(nums,i,j));
+            ans=Math.max(ans,sum);
         }
         return ans;
-    }
-    public int sum(int[]nums,int i,int j){
-        if(i==j)return nums[i];
-        int res=0;
-        while(i<j){
-            res+=nums[i];
-            i++;
-        }
-        return res;
     }
 }
