a8b9849223e90606290af40ff6c75af7a86f7637
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-06-14 11:48:54 +0530

    Time: 11 ms (75.83%), Space: 47.5 MB (13.71%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -1,18 +1,23 @@
-public class Solution {
-    public int minDistance(String s1, String s2) {
-        int[] dp = new int[s2.length() + 1];
-        for (int i = 0; i <= s1.length(); i++) {
-            int[] temp=new int[s2.length()+1];
-            for (int j = 0; j <= s2.length(); j++) {
-                if (i == 0 || j == 0)
-                    temp[j] = i + j;
-                else if (s1.charAt(i - 1) == s2.charAt(j - 1))
-                    temp[j] = dp[j - 1];
-                else
-                    temp[j] = 1 + Math.min(dp[j], temp[j - 1]);
+class Solution {
+    public int minDistance(String word1, String word2) {
+        
+        char[] s1= word1.toCharArray();
+        char[] s2 = word2.toCharArray();
+        
+        int[][] DP = new int[s1.length+1][s2.length+1];
+        
+        for (int i=s1.length-1;i>=0;--i){
+            for (int j=s2.length-1;j>=0;--j){
+                if(s1[i] == s2[j]){
+                    DP[i][j]= 1+ DP[i+1][j+1];
+                }else{
+                    DP[i][j]=Math.max(DP[i+1][j],DP[i][j+1]);
+                }
             }
-            dp=temp;
         }
-        return dp[s2.length()];
+        
+        return word1.length()+word2.length()-(2*DP[0][0]);
     }
+    
+    
 }
