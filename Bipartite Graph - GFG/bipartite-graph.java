fba377275af626bec4d32da478076c43e5941019
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-06-17 15:07:33 +0530

    Added solution - LeetHub

---
+++ Bipartite Graph - GFG/bipartite-graph.java
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] S = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(S[0]);
+            int E = Integer.parseInt(S[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for(int i = 0; i < V; i++){
+                adj.add(new ArrayList<Integer>());
+            }
+            for(int i = 0; i < E; i++){
+                String[] s = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(s[0]);
+                int v = Integer.parseInt(s[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isBipartite(V, adj);
+            if(ans)
+                System.out.println("1");
+            else System.out.println("0");
+       }
+    }
+}// } Driver Code Ends
+
+
+class Solution
+{
+    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>graph)
+    {
+        boolean[]vis=new boolean[100000];
+        boolean[]color=new boolean[100000];
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<graph.size();i++){
+            if(graph.get(i).size()==0 || vis[i])continue;
+            q.add(i);
+            vis[i]=true;
+            color[i]=true;
+            while(!q.isEmpty()){
+                int node=q.remove();
+                for(int neb:graph.get(node)){
+                    if(!vis[neb]){
+                        q.add(neb);
+                        vis[neb]=true;
+                        color[neb]=!color[node];
+                    }else if(color[neb]==color[node]){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
