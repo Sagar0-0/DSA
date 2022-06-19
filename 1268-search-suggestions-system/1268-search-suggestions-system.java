b82e4f87dd0118716dd43d4316a2ccf9b05d9383
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-06-19 18:29:23 +0530

    Time: 421 ms (5.00%), Space: 200.5 MB (5.00%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -0,0 +1,67 @@
+
+class Trie {
+    class Node {
+        boolean isWord = false;
+        List<Node> children = Arrays.asList(new Node[26]);
+    };
+    Node Root, curr;
+    List<String> resultBuffer;
+    
+    void dfsWithPrefix(Node curr, String word) {
+        if (resultBuffer.size() == 3)
+            return;
+        if (curr.isWord)
+            resultBuffer.add(word);
+
+        // Run DFS on all possible paths.
+        for (char c = 'a'; c <= 'z'; c++)
+            if (curr.children.get(c - 'a') != null)
+                dfsWithPrefix(curr.children.get(c - 'a'), word + c);
+    
+    }
+    
+    Trie() {
+        Root = new Node();
+    }
+    
+    void insert(String s) {
+
+        curr = Root;
+        for (char c : s.toCharArray()) {
+            if (curr.children.get(c - 'a') == null)
+                curr.children.set(c - 'a', new Node());
+            curr = curr.children.get(c - 'a');
+        }
+
+        // Mark this node as a completed word.
+        curr.isWord = true;
+    }
+    List<String> getWordsStartingWith(String prefix) {
+        curr = Root;
+        resultBuffer = new ArrayList<String>();
+        // Move curr to the end of prefix in its trie representation.
+        for (char c : prefix.toCharArray()) {
+            if (curr.children.get(c - 'a') == null)
+                return resultBuffer;
+            curr = curr.children.get(c - 'a');
+        }
+        dfsWithPrefix(curr, prefix);
+        return resultBuffer;
+    }
+};
+class Solution {
+    List<List<String>> suggestedProducts(String[] products,
+                                         String searchWord) {
+        Trie trie = new Trie();
+        List<List<String>> result = new ArrayList<>();
+        // Add all words to trie.
+        for (String w : products)
+            trie.insert(w);
+        String prefix = new String();
+        for (char c : searchWord.toCharArray()) {
+            prefix += c;
+            result.add(trie.getWordsStartingWith(prefix));
+        }
+        return result;
+    }
+};
