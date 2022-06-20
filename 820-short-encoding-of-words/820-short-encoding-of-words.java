e0a3bc50c5b01dd4099df50ae081f5a1087cb864
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-06-20 19:41:35 +0530

    Time: 29 ms (71.17%), Space: 56.9 MB (53.37%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -1,20 +1,44 @@
 class Solution {
     public int minimumLengthEncoding(String[] words) {
-        Arrays.sort(words,(a,b)->b.length()-a.length());
-        StringBuilder ans=new StringBuilder();
-        for(String s:words){
-            if(ans.indexOf(s)==-1){
-                ans.append(s);
-                ans.append("#");
-            }else{
-                int idx=ans.lastIndexOf(s);
-                int end=idx+s.length();
-                if(ans.charAt(end)!='#'){
-                    ans.append(s);
-                    ans.append("#");
-                }
+        Trie trie = new Trie();
+        int sum = 0;
+        Arrays.sort(words, (s1, s2) -> s2.length() - s1.length());
+        for (String word : words) {
+            boolean isNew = trie.insert(word);
+            if (isNew) {
+                sum += word.length() + 1;
             }
         }
-        return ans.length();
+        return sum;
     }
+}
+
+// 定义tire
+class Trie {
+    
+    TrieNode root;
+    
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public boolean insert(String word) {
+        boolean isNew = false;
+        TrieNode node = root;
+        for (int i = word.length() - 1; i >= 0; --i) {
+            int index = word.charAt(i) - 'a';
+            if (node.children[index] == null) {
+                isNew = true;
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        return isNew;
+    }
+}
+
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+
+    public TrieNode() {}
 }
