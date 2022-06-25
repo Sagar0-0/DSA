8ad05b40570068d8b1cf58e1ed191e54d0a3fe67
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-06-25 10:31:11 +0530

    Time: 2 ms (30.74%), Space: 54.8 MB (5.06%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean checkPossibility(int[] a) {
+        boolean found=false;
+        for(int i=0;i<a.length-1;i++){
+            if(a[i]>a[i+1]){
+                if(found)return false;
+                found=true;
+                boolean done=false;
+                if(i!=0){
+                    if(a[i+1]>=a[i-1]){
+                        a[i]=a[i+1];
+                    }
+                }else{
+                    a[i]=a[i+1];
+                    done=true;
+                }
+                if(!done){
+                    if(i+1==a.length-1){
+                        a[i+1]=a[i];
+                    }else{
+                        if(a[i+2]>a[i+1])
+                            a[i+1]=a[i+2];
+                    }
+                    if(a[i]>a[i+1])return false;
+                }
+            }
+        }
+        return true;
+    }
+}
