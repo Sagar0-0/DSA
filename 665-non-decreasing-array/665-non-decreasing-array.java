a7f72063febf8cb7cec03382fa1767297766eeb2
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-06-25 10:33:59 +0530

    Time: 1 ms (97.53%), Space: 54.3 MB (28.03%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -1,29 +1,29 @@
 class Solution {
-    public boolean checkPossibility(int[] a) {
-        boolean found=false;
-        for(int i=0;i<a.length-1;i++){
-            if(a[i]>a[i+1]){
-                if(found)return false;
-                found=true;
-                boolean done=false;
-                if(i!=0){
-                    if(a[i+1]>=a[i-1]){
-                        a[i]=a[i+1];
-                    }
-                }else{
-                    a[i]=a[i+1];
-                    done=true;
-                }
-                if(!done){
-                    if(i+1==a.length-1){
-                        a[i+1]=a[i];
-                    }else{
-                        a[i+1]=a[i+2];
-                    }
-                    if(a[i]>a[i+1])return false;
-                }
-            }
-        }
-        return true;
-    }
+	public boolean checkPossibility(int[] arr) {
+		int index = helper(arr);
+		if (index == -1) {
+			return true;
+		}
+		int temp = arr[index];
+		arr[index] = arr[index + 1];
+		if (helper(arr) == -1) {
+			return true;
+		}
+
+		arr[index] = temp;
+		arr[index + 1] = temp;
+		if (helper(arr) == -1) {
+			return true;
+		}
+		return false;
+	}
+
+	public int helper(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
