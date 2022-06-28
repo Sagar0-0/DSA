03e3f0e4c6ef6449537e4e1bced770cc728affe5
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-06-28 12:22:43 +0530

    Time: 17 ms (61.03%), Space: 54.1 MB (71.51%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,28 +1,23 @@
 class Solution {
     public int minDeletions(String s) {
-        // Store the frequency of each character
-        int[] frequency = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            frequency[s.charAt(i) - 'a']++;
+        Set<Integer> set = new HashSet<>();
+        int[] alpha = new int[26];
+        char[] arr =s.toCharArray();
+        for(char c:arr)
+        {
+            alpha[c-'a']++;
+        }
+        int count = 0;
+        for(int i=0;i<26;i++) {
+            if(alpha[i] != 0) {
+                while(set.contains(alpha[i]) && alpha[i]>0) {
+                    alpha[i]--;
+                    count++;
+                }
+                set.add(alpha[i]);
+            } 
         }
         
-        Arrays.sort(frequency);
-        
-        int deleteCount = 0;
-        // Maximum frequency the current character can have
-        int maxFreqAllowed = s.length();
-        
-        // Iterate over the frequencies in descending order
-        for (int i = 25; i >= 0 && frequency[i] > 0; i--) {
-            // Delete characters to make the frequency equal the maximum frequency allowed
-            if (frequency[i] > maxFreqAllowed) {
-                deleteCount += frequency[i] - maxFreqAllowed;
-                frequency[i] = maxFreqAllowed;
-            }
-            // Update the maximum allowed frequency
-            maxFreqAllowed = Math.max(0, frequency[i] - 1);
-        }
-        
-        return deleteCount;
-    }     
+        return count;
+    }
 }
