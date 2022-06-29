0078adab93731027653388ebb502f77ba4cbc67b
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-06-29 17:14:02 +0530

    Update Jump Game Array.java

--- Jump Game Array.java
+++ Jump Game Array.java
@@ -1,21 +1,12 @@
-public class Solution {
-    public int canJump(int[] A) {
-        int n=A.length;
-        boolean[] vis=new boolean[n];
-        vis[0]=true;
-        dfs(vis,A,0);
-        return vis[n-1]?1:0;
-    }
-    public void dfs(boolean[]vis,int[]A,int idx){
-        if(A[idx]==0)return;
-        int max=idx+A[idx];
-        int i=idx+1;
-        while(i<A.length && i<=max){
-            if(vis[i]==false){
-                dfs(vis,A,i);
-                vis[i]=true;
-            }
-            i++;
+class Solution {
+    static int canReach(int A[], int N) {
+        int step=0;
+        for(int i=N-2;i>=0;i--)
+        {
+            step++;
+            if(A[i]>=step)step=0;
         }
-    }
+        if(step==0)return 1;
+        return 0;
+    } 
 }
