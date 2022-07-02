e13e5f3022df1844515cb9d96d4df8328e0f8bbe
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-07-02 15:03:53 +0530

    Time: 14 ms (100.00%), Space: 52 MB (86.61%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -1,28 +1,14 @@
 class Solution {
-    public int maxArea(int h, int w, int[] a, int[] b) {
-        Arrays.sort(a);
-        Arrays.sort(b);
-        int mod=1000000007;
-        long hMax=0;
-        for(int i=0;i<=a.length;i++){
-            if(i==0){
-                hMax=Math.max(hMax,a[i]);
-            }else if(i==a.length){
-                hMax=Math.max(hMax,h-a[i-1]);
-            }else{
-                hMax=Math.max(hMax,a[i]-a[i-1]);
-            }
+    public int maxArea(int h, int w, int[] hc, int[] vc) {
+        return (int)((getMax(h,hc)*getMax(w,vc))%1000000007);
+    }
+    public long getMax(int len,int[] cuts){
+        Arrays.sort(cuts);
+        long from=0,max=0;
+        for(int c:cuts){
+            max=Math.max(max,c-from);
+            from=c;
         }
-        long vMax=0;
-        for(int i=0;i<=b.length;i++){
-            if(i==0){
-                vMax=Math.max(vMax,b[i]);
-            }else if(i==b.length){
-                vMax=Math.max(vMax,w-b[i-1]);
-            }else{
-                vMax=Math.max(vMax,b[i]-b[i-1]);
-            }
-        }
-        return (int)(hMax%mod * vMax%mod ) %mod;
+        return Math.max(max,len-from);
     }
 }
