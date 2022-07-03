1e07262dca9b8a3ed1aeca44b8cc65d028311bf3
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-07-03 21:09:36 +0530

    Added solution - LeetHub

--- Word Break - Part 2 - GFG/word-break-part-2.java
+++ Word Break - Part 2 - GFG/word-break-part-2.java
@@ -37,21 +37,24 @@ class GFG{
 //User function Template for Java
 
 class Solution{
-    static void dfs(List<String> dict, String s, String temp, List<String> solution){
-        if(s.length()==0)solution.add(temp.substring(0,temp.length()-1));
-        
-        for(int i=0; i<s.length(); i++){
-            String left = s.substring(0,i+1);
-            if(dict.contains(left)){
-                String right = s.substring(i+1);
-                dfs(dict, right, temp + left + " ", solution);
-            }
-        }
-    }
     static List<String> wordBreak(int n, List<String> dict, String s)
     {
-        List<String> solution = new ArrayList<>();
-        dfs(dict, s, "", solution);
-        return solution;
+        // code here
+        List<String> ans=new ArrayList<>();
+        dfs(dict,s,"",ans);
+        return ans;
+    }
+    static void dfs(List<String> dict,String s,String sentence,List<String> ans){
+        if(s.length()==0){
+            ans.add(sentence.substring(0,sentence.length()-1));
+        }
+        
+        for(int i=0;i<s.length();i++){
+            String word=s.substring(0,i+1);
+            if(dict.contains(word)){
+                String temp=s.substring(i+1);
+                dfs(dict,temp,sentence+word+" ",ans);
+            }
+        }
     }
 }
