1f8039abf357bdaf0ae0bda8c14a9f3d8bda0b35
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-07-04 14:30:08 +0530

    Time: 5 ms (45.91%), Space: 51.9 MB (44.36%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        int sum = candies[ratings.length - 1];
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+            sum += candies[i];
+        }
+        return sum;
+    }
+}
