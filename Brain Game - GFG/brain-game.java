d5d457bbc1678020c4b8da0ef9877de127e2b47a
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-07-07 02:33:21 +0530

    Added solution - LeetHub

--- Brain Game - GFG/brain-game.java
+++ Brain Game - GFG/brain-game.java
@@ -51,28 +51,34 @@ class Solution
     }
     
     
-    public int primeFactors(int n){
-        int primes=0;
-        while (n % 2 == 0) {
-            primes++;
-            n /= 2;
+    int primeFactors(int n)
+    {
+        // to store the count of prime factors
+        int ans = 0 ;
+        // Print the number of 2s that divide n
+        while (n % 2 == 0)
+        {
+            ans++;
+            n = n/2;
         }
- 
-        // n must be odd at this point.  So we can
-        // skip one element (Note i = i +2)
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+
+        // n must be odd at this point. So we can skip
+        // one element (Note i = i +2)
+        for (int i = 3; i <= Math.sqrt(n); i = i + 2)
+        {
             // While i divides n, print i and divide n
-            while (n % i == 0) {
-                primes++;
-                n /= i;
+            while (n % i == 0)
+            {
+                ans++;
+                n = n/i;
             }
         }
- 
-        // This condition is to handle the case whien
-        // n is a prime number greater than 2
+
+        // This condition is to handle the case when n
+        // is a prime number greater than 2
         if (n > 2)
-            primes++;
-        return primes;
+            ans++;
+        return ans;
     }
     boolean isPrime(int n){
  
