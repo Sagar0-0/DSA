5750f6fbece0f3ed78b14c315ef7592c0df01e6d
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-07-07 21:01:15 +0530

    Time: 1 ms (99.72%), Space: 41.9 MB (82.33%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,22 +1,35 @@
-public class Solution {
+class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s3.length() != s1.length() + s2.length()) {
+        boolean[][] canReach = new boolean[s1.length()][s2.length()];
+        return isInterleaveHelper(s1, s2, s3, 0, 0, 0, canReach);
+    }
+    
+    private boolean isInterleaveHelper(String s1, String s2, String s3, int i1, int i2, int i3, boolean[][] canReach) {
+        if (i3 == s3.length()) {
+            if (i1 == s1.length() && i2 == s2.length()) return true;
             return false;
         }
-        boolean dp[] = new boolean[s2.length() + 1];
-        for (int i = 0; i <= s1.length(); i++) {
-            for (int j = 0; j <= s2.length(); j++) {
-                if (i == 0 && j == 0) {
-                    dp[j] = true;
-                } else if (i == 0) {
-                    dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
-                } else if (j == 0) {
-                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
-                } else {
-                    dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
-                }
-            }
+        if (i1 == s1.length()) {
+            return s2.substring(i2, s2.length()).equals(s3.substring(i3, s3.length()));
         }
-        return dp[s2.length()];
+        if (i2 == s2.length()) {
+            return s1.substring(i1, s1.length()).equals(s3.substring(i3, s3.length()));
+        }
+        if (canReach[i1][i2]) return false;
+        canReach[i1][i2] = true;
+        boolean s1Match = (s1.charAt(i1) == s3.charAt(i3));
+        boolean s2Match = (s2.charAt(i2) == s3.charAt(i3));
+        if (s1Match && s2Match) {
+            return isInterleaveHelper(s1, s2, s3, i1 + 1, i2, i3 + 1, canReach) || isInterleaveHelper(s1, s2, s3, i1, i2 + 1, i3 + 1, canReach);
+        }
+        else if (s1Match) {
+            return isInterleaveHelper(s1, s2, s3, i1 + 1, i2, i3 + 1, canReach);
+        }
+        else if (s2Match) {
+            return isInterleaveHelper(s1, s2, s3, i1, i2 + 1, i3 + 1, canReach);
+        }
+        else {
+            return false;
+        }
     }
 }
