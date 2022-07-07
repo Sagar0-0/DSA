07acfb5a425ad06e81ee0ea20bfc096f70562d8a
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-07-07 20:57:56 +0530

    Time: 8 ms (35.85%), Space: 41.9 MB (80.17%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s3.length() != s1.length() + s2.length()) {
+            return false;
+        }
+        boolean dp[] = new boolean[s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = true;
+                } else if (i == 0) {
+                    dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                } else if (j == 0) {
+                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                } else {
+                    dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                }
+            }
+        }
+        return dp[s2.length()];
+    }
+}
