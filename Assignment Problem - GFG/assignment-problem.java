8e012b0e6e61f91c344d77ca1800dbeff3234251
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-07-13 18:45:36 +0530

    Added solution - LeetHub

--- Assignment Problem - GFG/assignment-problem.java
+++ Assignment Problem - GFG/assignment-problem.java
@@ -28,37 +28,40 @@ class GFG {
 //User function Template for Java
 
 class Solution {
-    static int assignmentProblem(int a[], int n) {
-        
+    // public static void main(String[] args) {
+    //     System.out.println(assignmentProblem(new int[]
+    //             {2, 1, 2, 9, 8, 1, 1, 1, 1},3));
+    // }
+    static int assignmentProblem(int a[], int n){
+
         //create matrix
         int[][]mat=new int[n][n];
         int idx=0;
         for(int i=0;i<n;i++)for(int j=0;j<n;j++)mat[i][j]=a[idx++];
-        
-        List<int[]> indices=dfs(n,mat);
-        
+
+        List<Integer> indices=dfs(n,mat);
+
         int ans=0;
         for(int i=0;i<indices.size();i++){
-            int x=indices.get(i)[0];
-            int y=indices.get(i)[1];
-            ans+=mat[x][y];
+            ans+=mat[indices.get(i)][i];
         }
         return ans;
     }
-    static List<int[]> dfs(int n,int[][]mat){
+
+    static List<Integer> dfs(int n,int[][]mat){
         int[] u=new int[n];
         int[] v=new int[n];
         int[] ind=new int[n];
         Arrays.fill(ind,-1);
-        
+
         for(int i=0;i<n;i++){
             int j=0;
             int[]links=new int[n];
             Arrays.fill(links,-1);
-            
+
             int[]mins=new int[n];
             Arrays.fill(mins,1000);
-            
+
             boolean[]vis=new boolean[n];
             int mark_i=i;
             int mark_j=-1;
@@ -96,16 +99,13 @@ class Solution {
             }
             ind[j]=i;
         }
-        
-        
-        List<int[]> ans=new ArrayList<>();
+
+
+        List<Integer> ans=new ArrayList<>();
         for(int i=0;i<n;i++){
-            int[] curr=new int[2];
-            curr[0]=ind[i];
-            curr[1]=i;
-            ans.add(curr);
+            ans.add(ind[i]);
         }
         return ans;
     }
-    
+
 };
