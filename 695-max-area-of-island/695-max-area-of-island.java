9817460259adaa9a44a0ea2ec01ac68b97f7829b
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-07-15 18:34:30 +0530

    Time: 2 ms (100.00%), Space: 47.8 MB (29.41%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,25 +1,27 @@
 class Solution {
-    int[][] grid;
-    boolean[][] seen;
-
-    public int area(int r, int c) {
-        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length ||
-                seen[r][c] || grid[r][c] == 0)
-            return 0;
-        seen[r][c] = true;
-        return (1 + area(r+1, c) + area(r-1, c)
-                  + area(r, c-1) + area(r, c+1));
-    }
-
     public int maxAreaOfIsland(int[][] grid) {
-        this.grid = grid;
-        seen = new boolean[grid.length][grid[0].length];
-        int ans = 0;
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[0].length; c++) {
-                ans = Math.max(ans, area(r, c));
+
+        int max=0;
+        for (int i=0;i<grid.length;i++){
+            for (int j=0 ;j<grid[0].length; j++){
+                if(grid[i][j]==1){
+                    int area=iarea(grid,i,j);
+                    if(area >max)
+                        max=area;
+                }
             }
         }
-        return ans;
+        return max;
+    }
+    public static int iarea(int [][]grid,int i,int j){
+        if(i<grid.length && j<grid[0].length && i>=0 &&  j>=0 && grid[i][j]==1){
+            grid[i] [j]   =0;
+            int up=iarea(grid,i-1,j);
+            int down=iarea(grid, i+1,j);
+            int left=iarea(grid, i,j-1);
+            int right=iarea(grid,i,j+1);
+            return 1+left+right+up+down;
+        }
+     return 0;       
     }
 }
