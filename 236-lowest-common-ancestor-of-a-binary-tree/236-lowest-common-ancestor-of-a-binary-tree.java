b1ef61e8c552102abde64b36b38c5e28f58caddb
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-07-26 16:40:40 +0530

    Time: 11 ms (49.59%), Space: 47 MB (70.82%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,41 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
+    TreeNode res = null; 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Map<TreeNode,TreeNode> map=new HashMap<>();
-        Set<TreeNode> set=new HashSet<>();
-        map.put(root,null);
-        dfs(root,map);
-        
-        TreeNode curr=p;
-        while(curr!=null){
-            set.add(curr);
-            curr=map.get(curr);
-        }
-        curr=q;
-        while(curr!=null){
-            if(set.contains(curr)){
-                return curr;
-            }
-            curr=map.get(curr);
-        }
-        return curr;
+        helper(root, p, q);
+        return res; 
     }
-    void dfs(TreeNode root,Map<TreeNode,TreeNode> map){
-        if(root.left!=null){
-            map.put(root.left,root);
-            dfs(root.left,map);
-        }
-        if(root.right!=null){
-            map.put(root.right,root);
-            dfs(root.right,map);
-        }
+    boolean helper(TreeNode root, TreeNode p, TreeNode q) { 
+        if(root == null) return false; 
+        int curr = root == p || root == q ? 1 : 0; 
+        int left = helper(root.left, p, q) ? 1 : 0;
+        int right = helper(root.right, p, q) ? 1 : 0; 
+        int sum = right + left + curr; 
+        if(sum == 2) res = root;
+        return sum == 1;
     }
 }
