1e094abbcac8451244b3746153307268046f1350
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-07-29 16:00:13 +0530

    Time: 1 ms (98.01%), Space: 43.3 MB (26.41%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.java
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -1,26 +1,18 @@
 class Solution {
-    public List<String> findAndReplacePattern(String[] words, String pattern) {
-        List<String> ans = new ArrayList();
-        for (String word: words)
-            if (match(word, pattern))
-                ans.add(word);
-        return ans;
+    public static List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> list=new LinkedList<>();
+        for(int i=0;i<words.length;i++){
+            if(checkPattern(words[i],pattern)){
+                list.add(words[i]);
+            }
+        }
+        return list;
     }
-
-    public boolean match(String word, String pattern) {
-        Map<Character, Character> M = new HashMap();
-        for (int i = 0; i < word.length(); i++) {
-            char w = word.charAt(i);
-            char p = pattern.charAt(i);
-            if (!M.containsKey(w)) M.put(w, p);
-            if (M.get(w) != p) return false;
+    private static boolean checkPattern(String word, String pattern){
+        for(int i=0;i<word.length();i++){
+            if(word.indexOf(word.charAt(i))!=pattern.indexOf(pattern.charAt(i)))
+                 return false;
         }
-
-        boolean[] seen = new boolean[26];
-        for (char p: M.values()) {
-            if (seen[p - 'a']) return false;
-            seen[p - 'a'] = true;
-        }
-        return true;
+        return  true;
     }
 }
