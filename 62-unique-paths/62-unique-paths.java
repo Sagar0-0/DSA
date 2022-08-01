c8b34a276236d6c7b314176ba6b67617a1685b03
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-08-01 14:07:31 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (98.27%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -5,7 +5,7 @@ class Solution {
     }
     private int helper(int m, int n, int[][] dp) {
         if(m<1 || n<1) return 0;
-        if(m == 1 && n == 1) return 1;
+        if(m == 1 || n == 1) return 1;
         if(dp[m][n] != 0) return dp[m][n];
         dp[m][n] = helper(m, n-1, dp) + helper(m-1, n, dp);
         return dp[m][n];
