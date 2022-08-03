9234efa7494f49d33e0ab6917c9704019e971511
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-08-03 14:07:56 +0530

    Time: 17 ms (98.84%), Space: 54.4 MB (65.61%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,17 +1,42 @@
 class MyCalendar {
-    TreeMap<Integer, Integer> calendar;
 
-    MyCalendar() {
-        calendar = new TreeMap();
+    class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        int start;
+        int end;
+        TreeNode(int start,int end){
+            this.start = start;
+            this.end = end;
+        }
     }
-
+    TreeNode root;
+    public MyCalendar() {
+       
+    }
+    
     public boolean book(int start, int end) {
-        Integer prev = calendar.floorKey(start),
-                next = calendar.ceilingKey(start);
-        if ((prev == null || calendar.get(prev) <= start) &&
-                (next == null || end <= next)) {
-            calendar.put(start, end);
-            return true;
+       if(root == null){
+           root = new TreeNode(start,end);
+           return true;
+       }
+       return insert(root,start,end);
+    }
+    
+    private boolean insert(TreeNode root, int start, int end){
+        if(end <= root.start){
+            if(root.left == null){
+                root.left = new TreeNode(start,end);
+                return true;
+            }
+            return insert(root.left,start,end);
+        }
+        if(start >= root.end){
+            if(root.right == null){
+                root.right = new TreeNode(start,end);
+                return true;
+            }
+            return insert(root.right,start,end);
         }
         return false;
     }
