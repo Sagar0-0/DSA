f330e6102cf844f8f94db619afec076d0040d497
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-08-03 14:04:00 +0530

    Time: 36 ms (60.99%), Space: 54.4 MB (58.86%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,36 +1,18 @@
 class MyCalendar {
-    PriorityQueue<int[]> pq;
-    public MyCalendar() {
-        pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+    TreeMap<Integer, Integer> calendar;
+
+    MyCalendar() {
+        calendar = new TreeMap();
     }
-    
+
     public boolean book(int start, int end) {
-        if(pq.size()==0){
-            pq.add(new int[]{start,end});
+        Integer prev = calendar.floorKey(start),
+                next = calendar.ceilingKey(start);
+        if ((prev == null || calendar.get(prev) <= start) &&
+                (next == null || end <= next)) {
+            calendar.put(start, end);
             return true;
         }
-        List<int[]> list=new LinkedList<>();
-        boolean toAdd=true;
-        while(!pq.isEmpty()){
-            int[] iv=pq.remove();
-            list.add(iv);
-            if(iv[0] < end && start < iv[1]){
-                toAdd=false;
-                break;
-            }
-        }
-        pq.addAll(list);
-        if(toAdd){
-            pq.add(new int[]{start,end});
-            return true;
-        }else{
-            return false;
-        }
+        return false;
     }
 }
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar obj = new MyCalendar();
- * boolean param_1 = obj.book(start,end);
- */
