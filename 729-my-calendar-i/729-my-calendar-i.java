06085e01ab2f3dcf8363eceac12ef14921129be4
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-08-03 14:03:11 +0530

    Time: 2213 ms (5.00%), Space: 118.1 MB (5.04%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,15 +1,36 @@
-public class MyCalendar {
-    List<int[]> calendar;
-
-    MyCalendar() {
-        calendar = new ArrayList();
+class MyCalendar {
+    PriorityQueue<int[]> pq;
+    public MyCalendar() {
+        pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
     }
-
+    
     public boolean book(int start, int end) {
-        for (int[] iv: calendar) {
-            if (iv[0] < end && start < iv[1]) return false;
+        if(pq.size()==0){
+            pq.add(new int[]{start,end});
+            return true;
         }
-        calendar.add(new int[]{start, end});
-        return true;
+        List<int[]> list=new LinkedList<>();
+        boolean toAdd=true;
+        while(!pq.isEmpty()){
+            int[] iv=pq.remove();
+            list.add(iv);
+            if(iv[0] < end && start < iv[1]){
+                toAdd=false;
+                break;
+            }
+        }
+        pq.addAll(list);
+        if(toAdd){
+            pq.add(new int[]{start,end});
+            return true;
+        }else{
+            return false;
+        }
     }
 }
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
