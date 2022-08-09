98c7b5d0a9560fbf9ffcce8ea4e5a71d3187923e
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-08-09 17:16:25 +0530

    Time: 20 ms (99.42%), Space: 46.3 MB (79.77%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.java
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.java
@@ -1,27 +1,29 @@
 class Solution {
-    public int numFactoredBinaryTrees(int[] A) {
-        int MOD = 1_000_000_007;
-        int N = A.length;
-        Arrays.sort(A);
-        long[] dp = new long[N];
-        Arrays.fill(dp, 1);
-
-        Map<Integer, Integer> index = new HashMap();
-        for (int i = 0; i < N; ++i)
-            index.put(A[i], i);
-
-        for (int i = 0; i < N; i++){
+    public int numFactoredBinaryTrees(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length, mod = 1_000_000_007;
+        long[] dp = new long[n];
+        long res = 0;
+        Map<Integer, Integer> idxs = new HashMap();
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
             for (int j = 0; j < i; j++) {
-                if (A[i] % A[j] == 0){ // A[j] is left child
-                    int right = A[i] / A[j];
-                    if (index.containsKey(right)){
-                        dp[i] = (dp[i] + dp[j] * dp[index.get(right)]) % MOD;
+                if (arr[j]*arr[j] > arr[i])break;
+                if (arr[i] % arr[j] == 0 && idxs.containsKey(arr[i]/arr[j])) {
+                    if (arr[j]*arr[j] == arr[i]) {
+                        dp[i] += dp[j]*dp[j];
+                        dp[i] %= mod;
+                    }
+                    else {
+                        dp[i] += 2*dp[j]*dp[idxs.get(arr[i]/arr[j])];
+                        dp[i] %= mod;
                     }
                 }
             }
+            idxs.put(arr[i], i);
+            res += dp[i];
+            res %= mod;
         }
-        long ans = 0;
-        for (long x: dp) ans += x;
-        return (int) (ans % MOD);
+        return (int) res;
     }
 }
