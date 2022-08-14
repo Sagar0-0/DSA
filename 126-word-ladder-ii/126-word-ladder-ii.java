0834622bdf5e40283bfd8ca7d72e17dc9eaf3334
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-08-14 15:09:39 +0530

    Time: 12 ms (92.93%), Space: 45.7 MB (69.48%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.java
+++ 126-word-ladder-ii/126-word-ladder-ii.java
@@ -1,67 +1,125 @@
 class Solution {
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        List<List<String>> ans = new ArrayList<>(); 
-        Map<String, Set<String>> reverse = new HashMap<>(); // reverse graph start from endWord
-        Set<String> wordSet = new HashSet<>(wordList); // remove the duplicate words
-        wordSet.remove(beginWord); // remove the first word to avoid cycle path
-        Queue<String> queue = new LinkedList<>(); // store current layer nodes
-        queue.add(beginWord); // first layer has only beginWord
-        Set<String> nextLevel = new HashSet<>(); // store nextLayer nodes
-        boolean findEnd = false; // find endWord flag
-        while (!queue.isEmpty()) { // traverse current layer nodes
-            String word = queue.remove();
-            for (String next : wordSet) {
-                if (isLadder(word, next)) { // is ladder words
-					// construct the reverse graph from endWord
-                    Set<String> reverseLadders = reverse.computeIfAbsent(next, k -> new HashSet<>());
-                    reverseLadders.add(word); 
-                    if (endWord.equals(next)) {
-                        findEnd = true;
+        this.beginWord = beginWord;
+        this.endWord = endWord;
+        if (!sanityCheck(wordList)) {
+            return new ArrayList<>();
+        }
+        prepare(wordList);
+        minTrans();
+        return trans;
+    }
+
+    private String beginWord;
+    private String endWord;
+    private Map<String, String[]> w2p;
+    private Map<String, List<String>> p2w;
+
+    private void minTrans() {
+        Map<String, Integer> minLens = new HashMap<>();
+        Map<String, Set<String>> prev = new HashMap<>();
+        List<String> queue = new ArrayList<>();
+        int minLen = -1;
+
+        minLens.put(endWord, 0);
+        queue.add(endWord);
+        int count = 0;
+        while (count < queue.size()) {
+            String node = queue.get(count++);
+            int len = minLens.get(node) + 1;
+            if (minLen != -1 && len > minLen) {
+                break;
+            }
+
+            for (String pattern : w2p.get(node)) {
+                for (String word : p2w.get(pattern)) {
+                    if (!minLens.containsKey(word)) {
+                        minLens.put(word, len);
+                        queue.add(word);
+                        prev.put(word, new HashSet<>());
+                        if (word.equals(beginWord)) {
+                            minLen = len;
+                        }
+                    } else if (minLens.get(word) < len) {
+                        continue;
                     }
-                    nextLevel.add(next); // store next layer nodes
+                    prev.get(word).add(node);
                 }
             }
-            if (queue.isEmpty()) { // when current layer is all visited
-                if (findEnd) break; // if find the endWord, then break the while loop
-                queue.addAll(nextLevel); // add next layer nodes to queue
-                wordSet.removeAll(nextLevel); // remove all next layer nodes in wordSet
-                nextLevel.clear();
-            }
         }
-        if (!findEnd) return ans; // if can't reach endWord from startWord, then return ans.
-        Set<String> path = new LinkedHashSet<>();
-        path.add(endWord);
-		// traverse reverse graph from endWord to beginWord
-        findPath(endWord, beginWord, reverse, ans, path); 
-        return ans;
+
+        makeTrans(prev, minLens.getOrDefault(beginWord, -1));
     }
 
+    private List<List<String>> trans;
+    private Map<String, Set<String>> paths;
 
-    private void findPath(String endWord, String beginWord, Map<String, Set<String>> graph,
-                                 List<List<String>> ans, Set<String> path) {
-        Set<String> next = graph.get(endWord);
-        if (next == null) return;
-        for (String word : next) {
-            path.add(word);
-            if (beginWord.equals(word)) {
-                List<String> shortestPath = new ArrayList<>(path);
-                Collections.reverse(shortestPath); // reverse words in shortest path
-                ans.add(shortestPath); // add the shortest path to ans.
-            } else {
-                findPath(word, beginWord, graph, ans, path);
+    private void makeTrans(Map<String, Set<String>> paths, int maxLen) {
+        this.trans = new ArrayList<>();
+        this.paths = paths;
+        if (maxLen == -1) {
+            return;
+        }
+        String[] path = new String[maxLen + 1];
+        path[0] = beginWord;
+        makeTransInternal(beginWord, 1, path);
+    }
+
+    void makeTransInternal(String start, int depth, String[] path) {
+        if (depth == path.length - 1) {
+            path[depth] = endWord;
+            List<String> result = new ArrayList<>();
+            for (String word : path) {
+                result.add(word);
             }
-            path.remove(word);
+            this.trans.add(result);
+            return;
+        }
+        for (String word : paths.get(start)) {
+            path[depth] = word;
+            makeTransInternal(word, depth + 1, path);
         }
     }
 
-    private boolean isLadder(String s, String t) {
-        if (s.length() != t.length()) return false;
-        int diffCount = 0;
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) != t.charAt(i)) diffCount++;
-            if (diffCount > 1) return false;
+    private boolean sanityCheck(List<String> wordList) {
+        for (String word : wordList) {
+            if (word.equals(endWord)) {
+                return true;
+            }
         }
-        return diffCount == 1;
+        return false;
+    }
+
+    private void prepare(List<String> wordList) {
+        // Prepare a word -> patterns map.
+        w2p = new HashMap<>();
+        w2p.put(beginWord, getPatterns(beginWord));
+        for (String word : wordList) {
+            w2p.put(word, getPatterns(word));
+        }
+
+        // Prepare a pattern -> words map.
+        p2w = new HashMap<>();
+        for (String word : w2p.keySet()) {
+            for (String pattern : w2p.get(word)) {
+                if (!p2w.containsKey(pattern)) {
+                    p2w.put(pattern, new ArrayList<>());
+                }
+                p2w.get(pattern).add(word);
+            }
+        }
+    }
+
+    private String[] getPatterns(String word) {
+        char[] wc = word.toCharArray();
+        char original;
+        String[] patterns = new String[wc.length];
+        for (int i = 0; i < wc.length; ++i) {
+            original = wc[i];
+            wc[i] = '*';
+            patterns[i] = new String(wc);
+            wc[i] = original;
+        }
+        return patterns;
     }
 }
