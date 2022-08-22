3c095e01716bfc9cf5e2fcbe298752975401e7b1
Adarsh Urmaliya <67074308+Satellite-system@users.noreply.github.com>
2022-08-23 03:11:04 +0530

    Without using set, time and space optimized
    Runtime: 78 ms, faster than 98.55% of C++ online submissions for 3Sum.
    Memory Usage: 19.7 MB, less than 99.13% of C++ online submissions for 3Sum.

--- 15. 3Sum/SolutionCode.cpp
+++ 15. 3Sum/SolutionCode.cpp
@@ -36,3 +36,31 @@ public:
         return v;
     }
 };
+
+
+// Second Better Approach : 
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for (unsigned int i=0; i<nums.size(); i++) {
+            if ((i>0) && (nums[i]==nums[i-1]))
+                continue;
+            int l = i+1, r = nums.size()-1;
+            while (l<r ) {
+                int s = nums[i]+nums[l]+nums[r];
+                if (s>0) r--;
+                else if (s<0) l++;
+                else {
+                    res.push_back(vector<int> {nums[i], nums[l], nums[r]});
+                    while (l < r && nums[l]==nums[l+1]) l++;
+                    while (l < r  && nums[r]==nums[r-1]) r--;
+                    l++; r--;
+                }
+            }
+        }
+        return res;
+    }
+};
