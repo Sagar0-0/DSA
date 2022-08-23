9ad621a02bac726c84f649aa4a4096b7eb77f7e6
Abhishek Kumar <68819998+Abhi2061@users.noreply.github.com>
2022-08-23 09:30:41 +0530

    Another approach added to the solution

--- Foldable Binary Tree.cpp
+++ Foldable Binary Tree.cpp
@@ -18,3 +18,43 @@ bool checkMirror(Node* root1, Node* root2)
     
     return checkMirror(root1->left, root2->right) and checkMirror(root1->right, root2->left);
 }
+
+////Another approach but takes more space than above solution
+
+/*
+    bool IsFoldable(Node* root)
+    {
+        // Your code goes here
+        queue<Node*> Q;
+        Q.push(root);
+
+        while(!Q.empty())
+        {
+            int sz = Q.size();
+            vector<bool> arr;
+
+            while(sz--)
+            {
+                Node* cur = Q.front();
+                Q.pop();
+
+                if(cur)
+                {
+                    arr.push_back(true);
+                    Q.push(cur->left);
+                    Q.push(cur->right);
+                }
+                else
+                    arr.push_back(false);
+            }
+
+            int n = arr.size();
+
+            for(int i=0; i<n/2; i++)
+                if(arr[i] ^ arr[n-i-1])
+                    return false;
+        }
+
+        return true;
+    }
+*/
