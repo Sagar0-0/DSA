29b30ed5775d16f63963843737fca46262567596
Aman Bhatt <87806212+bhattaman0001@users.noreply.github.com>
2022-08-28 21:45:12 +0530

    Update Binary Tree to DLL.cpp

--- Binary Tree to DLL.cpp
+++ Binary Tree to DLL.cpp
@@ -1,3 +1,52 @@
+// there are 2 approaches another one is below
+
+Node * bToDLL(Node *root)
+    {
+        // your code here
+        // TC: O(1) using morris inorder traversal
+
+        Node* curr = root, *head = NULL;
+
+        while(curr != NULL){
+            if(curr->left != NULL){
+                Node* traversal = curr->left;
+                while(traversal->right != NULL && traversal->right != curr)
+                    traversal = traversal->right;
+
+                if(traversal->right == NULL){
+                    traversal->right = curr;
+
+                    Node *tmp = curr->left;
+                    curr->left = traversal;
+                    curr=tmp;
+                    continue;
+                }
+            }    
+            if(head == NULL) head = curr;
+            //cout<<curr->right->data<<endl;
+
+            Node *traversal = curr->right;
+            if(traversal != NULL && traversal->left != curr){
+                while(traversal->left != NULL)
+                    traversal = traversal->left;
+
+                Node* tmp = curr->right;
+                traversal->left = curr;
+                curr->right = traversal;
+                curr = tmp;
+            }
+            else curr = curr->right;
+
+
+        }
+        return head;
+    }
+
+
+
+
+// 2. another approach
+// it is not complex just added comments that's why it is looking big
 struct Node
 {
     int data;
