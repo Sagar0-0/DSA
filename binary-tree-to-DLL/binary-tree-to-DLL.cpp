da1d163b17bcc585d1e071cb0c989c7f8e02c648
Aman Bhatt <87806212+bhattaman0001@users.noreply.github.com>
2022-08-28 11:10:44 +0530

    Create binary-tree-to-DLL.cpp

---
+++ binary-tree-to-DLL/binary-tree-to-DLL.cpp
@@ -0,0 +1,43 @@
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+// This function should return head to the DLL
+class Solution
+{
+    public: 
+    Node *p = NULL, *h = NULL;
+    void inorder(Node* root){
+        if(!root) return;
+        inorder(root->left);
+        root->left = p;
+        p = root;
+        inorder(root->right);
+    }
+    //Function to convert binary tree to doubly linked list and return it.
+    Node * bToDLL(Node *root)
+    {
+        // your code here
+        inorder(root); // making one traversal from left to right of doubly linkedList
+      
+      // making another traversal from right to left of DLL
+        Node* t = p, *p1 = NULL;
+        t->right = p1;
+        while(t->left != NULL){
+            p1 = t;
+            t = t->left;
+            t->right = p1;
+        }
+        h = t;
+        return h;
+    }
+};
