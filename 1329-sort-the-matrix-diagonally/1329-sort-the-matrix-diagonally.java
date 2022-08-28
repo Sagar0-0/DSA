73b19948327411409d4b5cd6ac146be8727ac863
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-08-28 15:16:04 +0530

    Time: 1 ms (100.00%), Space: 42.8 MB (99.22%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,47 +1,34 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
-        for(int row=0;row<mat.length;row++){
-            int limit=Math.min(mat.length-row,mat[0].length);
-            if(limit==1)continue;
-            ArrayList<Integer> arr=new ArrayList<>();
-            int x=row;
-            int y=0;
-            for(int j=0;j<limit;j++){
-                arr.add(mat[x][y]);
-                x++;
-                y++;
-            }
-            Collections.sort(arr);
-            x=row;
-            y=0;
-            for(int j=0;j<limit;j++){
-                mat[x][y]=arr.get(j);
-                x++;
-                y++;
-            }
+      int m=mat.length,n=mat[0].length;
+      //row=0
+        for(int col=0;col<n;col++){
+            sort(mat,0,col,m,n);
         }
-        
-        
-        for(int col=1;col<mat[0].length;col++){
-            int limit=Math.min(mat.length,mat[0].length-col);
-            if(limit==1)continue;
-            ArrayList<Integer> arr=new ArrayList<>();
-            int x=0;
-            int y=col;
-            for(int j=0;j<limit;j++){
-                arr.add(mat[x][y]);
-                x++;
-                y++;
-            }
-            Collections.sort(arr);
-            x=0;
-            y=col;
-            for(int j=0;j<limit;j++){
-                mat[x][y]=arr.get(j);
-                x++;
-                y++;
-            }
+        //col=0
+        for(int row=0;row<m;row++){
+            sort(mat,row,0,m,n);
         }
         return mat;
+    }
+    public static void sort(int[][] mat,int row,int col,int m,int n){
+        int[] values=new int[101];//using count sort
+        int r=row,c=col;
+        while(r<m&&c<n){
+            values[mat[r][c]]++;
+            r++;
+            c++;
+        }
+        r=row;
+        c=col;
+        for(int i=1;i<101;i++){
+            if(values[i]>0){
+                int cnt=values[i];
+                while(cnt-->0){
+                    mat[r][c]=i;
+                    r++;c++;
+                }
+            }
+        }
     }
 }
