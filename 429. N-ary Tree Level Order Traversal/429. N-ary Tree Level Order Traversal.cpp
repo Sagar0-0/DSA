ef4d9a841e7acbf0105ec98b80218272cbe4f604
Aman Bhatt <87806212+bhattaman0001@users.noreply.github.com>
2022-09-05 10:52:09 +0530

    Create 429. N-ary Tree Level Order Traversal.cpp

---
+++ 429. N-ary Tree Level Order Traversal/429. N-ary Tree Level Order Traversal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        if(!root) return {};
+        vector<vector<int>> ans;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> v;
+            for(int i=0;i<n;i++){
+                Node* node = q.front();q.pop();
+                v.push_back(node->val);
+              // just instead of left, right we are pushing childrens of node if found not null
+                for(Node* child : node->children) q.push(child);
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
