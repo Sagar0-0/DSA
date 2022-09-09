e5115dae587673bf6cb8fe8031505cb968fbfd46
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-09-09 21:51:38 +0530

    Update 1996-the-number-of-weak-characters-in-the-game.java

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -32,3 +32,43 @@ class Solution {
         
     }
 }
+
+
+//dp
+class Solution {
+    public int numberOfWeakCharacters(int[][] properties) {
+        int maxAttack = 0;
+        // Find the maximum attack value
+        for (int[] property : properties) {
+            int attack = property[0];
+            maxAttack = Math.max(maxAttack, attack);
+        }
+        
+        int maxDefense[] = new int[maxAttack + 2];
+        // Store the maximum defense for an attack value
+        for (int[] property : properties) {
+            int attack = property[0];
+            int defense = property[1];
+            
+            maxDefense[attack] = Math.max(maxDefense[attack], defense);
+        }
+
+        // Store the maximum defense for attack greater than or equal to a value
+        for (int i = maxAttack - 1; i >= 0; i--) {
+            maxDefense[i] = Math.max(maxDefense[i], maxDefense[i + 1]);
+        }
+        
+        int weakCharacters = 0;
+        for (int[] property : properties) {
+            int attack = property[0];
+            int defense = property[1];
+            
+            // If their is a greater defense for properties with greater attack
+            if (defense < maxDefense[attack + 1]) {
+                weakCharacters++;
+            }
+        }
+        
+        return weakCharacters;
+    }
+}
