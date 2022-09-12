ba672334ca4941c8cb4186c10adc95d57ad57171
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-09-12 19:47:53 +0530

    Update Minimum Exchange.java

--- Minimum Exchange.java
+++ Minimum Exchange.java
@@ -1,45 +1,45 @@
 class Solution
 {
-    public int MinimumExchange(char[][] matrix)
-    {
-        
-        int m = matrix.length , n = matrix[0].length;
-        int seq1 = 0 , seq2 = 0;
-        
-        for(int i = 0 ; i < m ; i++){
-            
-            for(int j = 0 ; j < n ; j++){
-                
+    public int MinimumExchange(char[][] matrix) {
+        int a = 0, b = 0;
+        int m = matrix.length, n = matrix[0].length;
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
                 if(matrix[i][j] == 'A'){
-                    
-                    if((i+j)%2 == 1){
-                        
-                        seq1++;
-                    }
-                    
-                    else{
-                        
-                       seq2++; 
-                    }
-                    
-                }
-                
-                else{
-                    
-                    if((i+j)%2 == 1){
-                        
-                        seq2++;
-                    }
-                    
-                    else{
-                        
-                        seq1++;
-                    }
+                    a++;
+                } else {
+                    b++;
                 }
             }
         }
-        
-        return Math.min(seq1 , seq2);
+        if(a > b){
+            return Count('A', 'B', matrix);
+        } else if(a < b){
+            return Count('B', 'A', matrix) ;
+        } else {
+            return Math.min(Count('A', 'B', matrix), Count('B', 'A', matrix));
+        }
     }
-    
+
+    private int Count(char x, char y, char[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int cnt = 0;
+        for(int i = 0; i < m; ++i){
+            char ch = x;
+            if(i % 2 == 1){
+                ch = y;
+            }
+            for(int j = 0; j < n; ++j){
+                if(matrix[i][j] != ch){
+                    cnt++;
+                }
+                if(ch == x){
+                    ch = y;
+                } else {
+                    ch = x;
+                }
+            }
+        }
+        return cnt / 2;
+    }
 }
