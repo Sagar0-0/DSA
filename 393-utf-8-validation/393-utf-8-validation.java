4e9685ed0b2a10733e61125b03db338207ef6b5a
Gajjala Harsha <gvsshhrao1998@gmail.com>
2022-09-13 21:12:48 +0530

    Update 393-utf-8-validation.java

--- 393-utf-8-validation/393-utf-8-validation.java
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -26,3 +26,55 @@ class Solution {
 	return isValid;
     }
 }
+
+
+//Approach 2
+
+
+class Solution {
+    public boolean validUtf8(int[] data) {
+        int f[]=new int[256];
+        for(int i=0;i<256;i++) {   // storing All sequences in in array having data[i] as index. As 0<=data[i]<=255, stored in f array
+            int count=0;
+            for(int j=7;j>=0;j--) {
+                if((i&(1<<j))>0) {
+                    count+=1;
+                }
+                else {
+                    break;
+                }
+            }
+           
+            if(count<=4) f[i]=count; // if starting 1's count <=4 storing in f array
+            else f[i]=-1;  //if starting 1's count>=5, it is not part of any utf-8 sequence making as -1
+        }
+        
+        int bytes=-1;
+        int sequence=-1;
+        for(int i=0;i<data.length;i++) {
+            int setBits=f[data[i]];
+            if(setBits==-1) return false;  //if particular value at index equals -1, then returning false as it might not be part of any utf-8 sequence
+            
+            if(bytes==-1 || sequence==0 || sequence == -1 ) {   //if one sequence completed i.e , 2 having 11000000 10000000 10000000 then sequence becomes 0 or if 1->00000000 then sequence becomes -1
+                bytes=f[data[i]];
+                if(bytes==1 && (data[i]&(1<<7))>0) {
+                    return false;
+                }
+                sequence=bytes-1;
+                continue;
+            }          
+            
+            if(setBits==1) { // After having starting , remaining sequence should contain only 1's
+                sequence-=1;
+            }
+            else { // otherwise returning false
+                return false;
+            }
+        }
+      
+        if(sequence==0 || sequence == -1) {   //At end sequence should be either 0 or -1 which means sequence completed for 0/1/2/3. for 0->sequence becomes -1, for 1,2,3->sequence becomes 0
+             return true;
+        }
+        return false;
+    }
+}
