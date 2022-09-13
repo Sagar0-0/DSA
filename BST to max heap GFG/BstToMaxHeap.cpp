0eb640fcfd69e3ea6bcca315d048109b946b6da9
Kunal Mittal <kunalmittal0107@gmail.com>
2022-09-13 06:03:08 +0530

    POTD of 13th september added

---
+++ BST to max heap GFG/BstToMaxHeap.cpp
@@ -0,0 +1,31 @@
+class Solution{
+  public:
+  // Inorder traversal
+  void helper1(Node *root,vector<int>&ans){
+      if(root==NULL){
+          return;
+      }
+      helper1(root->left,ans);
+      ans.push_back(root->data);
+      helper1(root->right,ans);
+  }
+  // Post order traversal
+  void helper2(Node *root,vector<int>&ans,int &index){
+      if(root==NULL)return;
+      helper2(root->left,ans,index);
+      helper2(root->right,ans,index);
+      root->data=ans[index];
+      index++;
+  
+      
+  }
+    void convertToMaxHeapUtil(Node* root)
+    {
+        // Your code goes here
+        vector<int>ans;
+        helper1(root,ans);
+        int index=0;
+        helper2(root,ans,index);
+        
+    }    
+};
