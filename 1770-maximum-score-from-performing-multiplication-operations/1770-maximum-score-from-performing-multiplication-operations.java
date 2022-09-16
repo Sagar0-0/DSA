d4f9a978e3fc32982fe3ee2c00ffd8686d8b79cb
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-09-16 18:57:49 +0530

    Time: 63 ms (84.53%), Space: 103.8 MB (73.33%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -6,9 +6,6 @@ class Solution {
         int m = multipliers.length;
         int[][] dp = new int[m + 1][m + 1];
         
-        for (int i = 0; i <= m; i++)
-            Arrays.fill(dp[i], 0);
-        
         for (int op = m - 1; op >= 0; op--) {
             for (int left = op; left >= 0; left--) {
                 dp[op][left] = Math.max(multipliers[op] * nums[left] + dp[op + 1][left + 1],
