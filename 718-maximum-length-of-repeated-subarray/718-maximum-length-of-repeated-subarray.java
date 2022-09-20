a05ee9c7cd8538295486b822bd5790de2206567a
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-09-20 21:05:28 +0530

    Time: 99 ms (30.61%), Space: 78.4 MB (39.98%) - LeetHub

---
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findLength(int[] A, int[] B) {
+        int ans = 0;
+        int[][] memo = new int[A.length + 1][B.length + 1];
+        for (int i = A.length - 1; i >= 0; --i) {
+            for (int j = B.length - 1; j >= 0; --j) {
+                if (A[i] == B[j]) {
+                    memo[i][j] = memo[i+1][j+1] + 1;
+                    if (ans < memo[i][j]) ans = memo[i][j];
+                }
+            }
+        }
+        return ans;
+    }
+}
