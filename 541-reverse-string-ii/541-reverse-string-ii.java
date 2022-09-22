1488dfd14b68e527ab7c99c3b23c594275ef8363
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-09-22 11:10:15 +0530

    Time: 2 ms (75.20%), Space: 44 MB (58.91%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.java
+++ 541-reverse-string-ii/541-reverse-string-ii.java
@@ -1,29 +1,36 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        boolean rev=true;
+        StringBuilder sb=new StringBuilder();
         int i=0;
-        StringBuilder ans=new StringBuilder();
-        while(i<s.length()){
-            if(i+k>s.length()){    
-                if(rev){
-                    StringBuilder sb=new StringBuilder(s.substring(i));
-                    sb.reverse();
-                    ans.append(sb);
-                }else{
-                    ans.append(s.substring(i));
-                }
-            }else{
-                if(rev){
-                    StringBuilder sb=new StringBuilder(s.substring(i,i+k));
-                    sb.reverse();
-                    ans.append(sb);
-                }else{
-                    ans.append(s.substring(i,i+k));
-                }
-                rev=!rev;
+        if(k>s.length())
+            return sb.append(s).reverse().toString();
+        boolean flag=true;
+        for(i=0;i<s.length();)
+        {
+            if(i+k<=s.length())
+            {
+                StringBuilder str=new StringBuilder(s.substring(i,i+k));
+                str.reverse();
+                sb.append(str);
+                flag=false;
+                i+=k;
+            }
+            if(i+k<=s.length())
+            {
+                sb.append(s.substring(i,i+k));
+                flag=true;
             }
             i+=k;
         }
-        return ans.toString();
+        
+        if(sb.length()<s.length()&&flag==false)
+            sb.append(s.substring(sb.length(),s.length()));
+        else if(sb.length()<s.length()&&flag==true)
+        {
+            StringBuilder str=new StringBuilder(s.substring(sb.length(),s.length()));
+                str.reverse();
+                sb.append(str);
+        }
+        return sb.toString();
     }
 }
