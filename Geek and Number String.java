b8ae993468a81100852691ce55fed0cb844c48a0
Gajjala Harsha <gvsshhrao1998@gmail.com>
2022-09-23 20:15:25 +0530

    Update Geek and Number String.java
    Made another solution using hashset and stack

--- Geek and Number String.java
+++ Geek and Number String.java
@@ -18,3 +18,41 @@ class Solution {
     	return st.size();
     }
 }
+
+
+//Method2 using hashset and stack
+
+class Solution { 
+    public int minLength(String s, int n) { 
+        HashSet<Integer> hs=new HashSet<>();
+        hs.add(12);
+        hs.add(21);
+        hs.add(34);
+        hs.add(43);
+        hs.add(56);
+        hs.add(65);
+        hs.add(78);
+        hs.add(87);
+        hs.add(9);
+        hs.add(90);
+        
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++) {
+            st.add(s.charAt(i)-'0');
+            
+            Integer top=-1;
+            Integer prevTop=-1;
+            while(st.size()>=2 && ((top==-1 && prevTop==-1) || hs.contains(prevTop*10+top))) {
+                top=st.pop();
+                prevTop=st.pop();
+            }
+            if((top!=-1 && prevTop!=-1) && !hs.contains(prevTop*10+top)) {
+                st.add(prevTop);
+                st.add(top);
+            }
+        }
+        
+        return st.size();
+
+    }
+}
