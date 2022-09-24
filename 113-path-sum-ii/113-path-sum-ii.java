9fb7595f70007e92f39f7e8793224470f8403814
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-09-24 12:25:52 +0530

    Time: 2 ms (85.41%), Space: 45.4 MB (17.42%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -1,42 +1,24 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> list=new ArrayList<>();
-        dfs(0,list,ans,targetSum,root);
-        return ans;
-    }
-    void dfs(int currSum,List<Integer> list,List<List<Integer>> ans,int target,TreeNode root){
-        if(root==null)return;
-        if(root.left==null && root.right==null){
-            list.add(root.val);
-            currSum+=root.val;
-            if(currSum==target && list.size()>0){
-                ans.add(new ArrayList<>(list));
-            }
-            list.remove(list.size()-1);
-            currSum-=root.val;
+    public void check(TreeNode root,int T,List<Integer> curr,List<List<Integer>> list){
+        if(root == null){
             return;
         }
-        list.add(root.val);
-        currSum+=root.val;
-        if(root.left!=null)dfs(currSum,list,ans,target,root.left);
-        if(root.right!=null)dfs(currSum,list,ans,target,root.right);
-        list.remove(list.size()-1);
-        currSum-=root.val;
+        if(T-root.val==0 && root.left == root.right){
+            curr.add(root.val);
+            list.add(new ArrayList<>(curr));
+            curr.remove(curr.size()-1);
+            return;
+        }
+        curr.add(root.val);
+        check(root.left,T-root.val,curr,list);
+        check(root.right,T-root.val,curr,list);
+        curr.remove(curr.size()-1);
+        
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        check(root,targetSum,curr,list);
+        return list;
     }
 }
