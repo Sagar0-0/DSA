33581b539963e047c4b3250423d8e2483aa93bbc
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-09-27 22:47:56 +0530

    Time: 54 ms (18.10%), Space: 54 MB (74.87%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.java
+++ 838-push-dominoes/838-push-dominoes.java
@@ -1,35 +1,30 @@
 class Solution {
-    public String pushDominoes(String dominoes) {
-        int N = dominoes.length();
-        int[] indexes = new int[N+2];
-        char[] symbols = new char[N+2];
-        int len = 1;
-        indexes[0] = -1;
-        symbols[0] = 'L';
+    public String pushDominoes(String S) {
+        char[] A = S.toCharArray();
+        int N = A.length;
+        int[] forces = new int[N];
 
-        for (int i = 0; i < N; ++i)
-            if (dominoes.charAt(i) != '.') {
-                indexes[len] = i;
-                symbols[len++] = dominoes.charAt(i);
-            }
-
-        indexes[len] = N;
-        symbols[len++] = 'R';
-
-        char[] ans = dominoes.toCharArray();
-        for (int index = 0; index < len - 1; ++index) {
-            int i = indexes[index], j = indexes[index+1];
-            char x = symbols[index], y = symbols[index+1];
-            char write;
-            if (x == y) {
-                for (int k = i+1; k < j; ++k)
-                    ans[k] = x;
-            } else if (x > y) { // RL
-                for (int k = i+1; k < j; ++k)
-                    ans[k] = k-i == j-k ? '.' : k-i < j-k ? 'R' : 'L';
-            }
+        // Populate forces going from left to right
+        int force = 0;
+        for (int i = 0; i < N; ++i) {
+            if (A[i] == 'R') force = N;
+            else if (A[i] == 'L') force = 0;
+            else force = Math.max(force - 1, 0);
+            forces[i] += force;
         }
 
-        return String.valueOf(ans);
+        // Populate forces going from right to left
+        force = 0;
+        for (int i = N-1; i >= 0; --i) {
+            if (A[i] == 'L') force = N;
+            else if (A[i] == 'R') force = 0;
+            else force = Math.max(force - 1, 0);
+            forces[i] -= force;
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (int f: forces)
+            ans.append(f > 0 ? 'R' : f < 0 ? 'L' : '.');
+        return ans.toString();
     }
 }
