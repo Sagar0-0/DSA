3662e137d4f522829ac6ee7b5182c5b2cd168f94
ervaibhavkumar <vk530873@gmail.com>
2022-10-01 09:21:06 +0530

    ervaibhavkumar Adding code for Binary Search in Python

---
+++ 704-binary-search/704-Binary-Search.py
@@ -0,0 +1,25 @@
+# Binary search logic in python 
+
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+ 
+    while low <= high:
+ 
+        mid = (high + low) // 2
+ 
+        # If x is greater, ignore left half
+        if arr[mid] < x:
+            low = mid + 1
+ 
+        # If x is smaller, ignore right half
+        elif arr[mid] > x:
+            high = mid - 1
+ 
+        # means x is present at mid
+        else:
+            return mid
+ 
+    # If we reach here, then the element was not present
+    return -1
