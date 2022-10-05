8868ea8a6d29250422e4f7d626ab76b272483ee1
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-10-05 19:54:51 +0530

    Time: 1 ms (88.59%), Space: 45.5 MB (46.28%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -4,38 +4,38 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-public class Solution {
-    public TreeNode addOneRow(TreeNode t, int v, int d) {
-        if (d == 1) {
-            TreeNode n = new TreeNode(v);
-            n.left = t;
-            return n;
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if(depth==1){
+            TreeNode head=new TreeNode(val);
+            head.left=root;
+            return head;
         }
-        Queue < TreeNode > queue = new LinkedList < > ();
-        queue.add(t);
-        int depth = 1;
-        while (depth < d - 1) {
-            Queue < TreeNode > temp = new LinkedList < > ();
-            while (!queue.isEmpty()) {
-                TreeNode node = queue.remove();
-                if (node.left != null) temp.add(node.left);
-                if (node.right != null) temp.add(node.right);
-            }
-            queue = temp;
-            depth++;
+        dfs(root,val,depth-1);
+        return root;
+    }
+    public void dfs(TreeNode root,int val,int depth){
+        if(depth==1){
+            TreeNode lefty=root.left;
+            TreeNode righty=root.right;
+            TreeNode newLeft=new TreeNode(val);
+            TreeNode newRight=new TreeNode(val);
+            newLeft.left=lefty;
+            root.left=newLeft;
+            newRight.right=righty;
+            root.right=newRight;
+            return;
         }
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.remove();
-            TreeNode temp = node.left;
-            node.left = new TreeNode(v);
-            node.left.left = temp;
-            temp = node.right;
-            node.right = new TreeNode(v);
-            node.right.right = temp;
-        }
-        return t;
+        if(root.left!=null)dfs(root.left,val,depth-1);
+        if(root.right!=null)dfs(root.right,val,depth-1);
     }
 }
