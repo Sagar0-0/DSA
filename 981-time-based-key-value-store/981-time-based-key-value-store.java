da6f416398803b580a5b1ae719cc0ceece1eae01
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-10-06 21:56:55 +0530

    Time: 449 ms (10.81%), Space: 216.4 MB (66.63%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,32 +1,75 @@
-class TimeMap {
-    Map<String,Map<Integer,String>> map;
-    public TimeMap() {
-        map=new HashMap<>();
+class TimeValue {
+    
+    public int time;
+    public String value;
+    
+    public TimeValue(int time, String value) {
+        
+        this.time = time;
+        this.value = value;
     }
     
-    public void set(String key, String value, int timestamp) {
-        map.putIfAbsent(key,new HashMap<>());
-        map.get(key).put(timestamp,value);
-    }
-    
-    public String get(String key, int timestamp) {
-        if(map.containsKey(key)){
-            Map<Integer,String> curr=map.get(key);
-            while(timestamp>=1){
-                if(curr.containsKey(timestamp)){
-                    return curr.get(timestamp);
-                }
-                timestamp--;
-            }
-            return "";
-        }
-        return "";
+    public String toString() {
+        
+        return "TimeValue with time " + this.time 
+            + " and value " + this.value;
     }
 }
 
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap obj = new TimeMap();
- * obj.set(key,value,timestamp);
- * String param_2 = obj.get(key,timestamp);
- */
+class TimeMap {
+    
+    private Map<String, List<TimeValue>> map = new HashMap<>();
+
+    public TimeMap() {
+        
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        
+        // Add the given (key, timestamp) combination 
+        
+        TimeValue newTimeValue = new TimeValue(timestamp, value);
+        
+        if (!this.map.containsKey(key)) {
+            
+            this.map.put(key, new ArrayList<>());
+        }
+        
+        this.map.get(key).add(newTimeValue);
+        
+        // System.out.println(this.map.get(key));
+    }
+    
+    public String get(String key, int timestamp) {
+        
+        List<TimeValue> list = this.map.get(key);
+        
+        if (list == null) {
+            
+            return "";
+        }
+        
+        // Binary search 
+        int left = 0;
+        int right = list.size() - 1;
+        
+        TimeValue best = null;
+        
+        while (left <= right) {
+            
+            int mid = left + (right - left) / 2;
+            
+            if (list.get(mid).time <= timestamp) {
+                
+                // Valid time 
+                best = list.get(mid);
+                left = mid + 1;
+            } else {
+                
+                right = mid - 1;
+            }
+        }
+        
+        return (best != null ? best.value : "");
+    }
+}
