d721e5875bac82497d95ee6d461fd49897d80d9f
Jayaprakash Sahoo <aryakumarjayaprakash@gmail.com>
2022-10-09 18:14:18 +0530

    Create Help Ishaan.java
    The solution for Problem of the day | 09-10-2022

---
+++ GFG/Help Ishaan/Help Ishaan.java
@@ -0,0 +1,30 @@
+class Solution
+{
+    public int NthTerm(int n)
+    {
+        // code here
+        int diff = 0;
+        if(isPrime(n)){
+            return diff;
+        }
+        while(true){
+            if(isPrime(n+diff) || isPrime(n-diff)){
+                break;
+            }
+            diff++;
+        }
+        return diff;
+        
+    }
+    public static Boolean isPrime(int num){
+        if(num<2){
+            return false;
+        }
+        for(int i = 2; i<= Math.sqrt(num); i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
