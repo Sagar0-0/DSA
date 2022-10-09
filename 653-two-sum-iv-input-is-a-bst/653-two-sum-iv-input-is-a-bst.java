ea48b8a8c131e15472006eeb3dcea4512c62e913
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-10-09 18:35:50 +0530

    Time: 3 ms (97.13%), Space: 50.2 MB (76.68%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -1,35 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    boolean flag;
-    Set<Integer> set;
+public class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        set=new HashSet<>();
-        flag=false;
-        dfs(root,k);
-        return flag;
+        return dfs(root, root, k);
     }
-    void dfs(TreeNode root,int k){
-        if(root==null)return;
-        if(set.contains(k-root.val)){
-            flag=true;
-            return;
-        }
-        set.add(root.val);
-        dfs(root.left,k);
-        dfs(root.right,k);
+    private boolean dfs(TreeNode root, TreeNode cur, int target) {
+        if(cur == null) return false;
+        return search(root, cur, target - cur.val) ||
+               dfs(root, cur.left, target) ||
+               dfs(root, cur.right, target);
+    }
+    private boolean search(TreeNode root, TreeNode cur, int target) {
+        if(root == null) return false;
+        return (root.val == target && cur != root)||
+               (target > root.val && search(root.right, cur, target)) || 
+               (target < root.val && search(root.left, cur, target));
     }
 }
