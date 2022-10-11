1f8a09f8aa705b18dcbef7633610e9c8255a3e30
Gajjala Harsha <gvsshhrao1998@gmail.com>
2022-10-11 21:51:31 +0530

    Update Decode the string.java
    Using StringBuilder with single stack

--- GFG/Decode the string/Decode the string.java
+++ GFG/Decode the string/Decode the string.java
@@ -40,3 +40,48 @@ class Solution{
         return sb.reverse().toString();
     }
 }
+
+
+//Method 2 Using single stack
+
+
+class Solution{
+    static String decodedString(String s){
+        // code here
+        Stack<StringBuilder> st=new Stack<>();
+        
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i)!=']') {
+                StringBuilder sb1=new StringBuilder(""+s.charAt(i));
+                st.push(sb1);
+            }
+            else {
+                StringBuilder sb=new StringBuilder();
+                StringBuilder num=new StringBuilder();
+                while(!st.isEmpty() && (st.peek().length()>1 || (st.peek().length()==1 && st.peek().charAt(0)!='['))) { //popping all and adding to sb until we come across '['
+                    sb.append(st.pop());
+                }
+                st.pop();
+                while(!st.isEmpty() && 
+                    (st.peek().length()==1 && st.peek().charAt(0)>='0' && st.peek().charAt(0)<='9')) { 
+                    num.append(""+st.pop());
+                }
+               
+                int times=1;
+                if(num.length()>0)
+                    times=Integer.valueOf(num.reverse().toString());
+                    
+                StringBuilder toAppend=new StringBuilder();
+                for(int j=0;j<times;j++) {
+                    toAppend.append(sb);
+                }
+                st.push(toAppend);
+            }
+        }
+        StringBuilder res=new StringBuilder();
+        while(!st.isEmpty()) {
+            res.append(st.pop());
+        }
+        return res.reverse().toString();
+    }
+}
