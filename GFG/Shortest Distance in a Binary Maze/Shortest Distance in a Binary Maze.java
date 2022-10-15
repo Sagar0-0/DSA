2cfd9d0e1331ac04dee12a1bd83a49cb1f7036a9
Gajjala Harsha <gvsshhrao1998@gmail.com>
2022-10-15 22:54:10 +0530

    Update Shortest Distance in a Binary Maze.java
    Another Approach

--- GFG/Shortest Distance in a Binary Maze/Shortest Distance in a Binary Maze.java
+++ GFG/Shortest Distance in a Binary Maze/Shortest Distance in a Binary Maze.java
@@ -42,3 +42,53 @@ class Solution {
         return -1;
     }
 }
+
+
+//Approach 2
+
+
+class Solution {
+    class Pair {
+        int i, j;
+        Pair(int i, int j) {
+            this.i=i;
+            this.j=j;
+        }
+    }
+    int shortestPath(int[][] grid, int[] source, int[] destination) {
+        int visited[][]=new int[grid.length][grid[0].length];
+        Queue<Pair> q=new LinkedList<>();
+        visited[source[0]][source[1]]=1;
+        if(grid[source[0]][source[1]] == 0) return -1;
+        q.add(new Pair(source[0], source[1]));
+        int ans=0;
+        while(!q.isEmpty()) {
+            int n=q.size();
+            while(n>0) {
+                Pair p=q.poll();
+                if(p.i==destination[0] && p.j==destination[1]) return ans;
+                
+                Pair up=getPair(p.i, p.j+1, grid, visited);
+                Pair down=getPair(p.i, p.j-1, grid, visited);
+                Pair left=getPair(p.i-1, p.j, grid, visited);
+                Pair right=getPair(p.i+1, p.j, grid, visited);
+                
+                if(up!=null) q.add(up);
+                if(down!=null) q.add(down);
+                if(left!=null) q.add(left);
+                if(right!=null) q.add(right);
+                n-=1;
+            }
+            ans+=1;
+        }
+        return -1;
+    }
+    public Pair getPair(int x, int y, int[][] grid, int [][]visited) {
+        if(x<0 || y<0 || x>=grid.length || y>=grid[0].length) return null;
+        if(grid[x][y]==0 || visited[x][y]==1) {
+            return null;
+        }
+        visited[x][y]=1;        
+        return new Pair(x, y);
+    }
+}
