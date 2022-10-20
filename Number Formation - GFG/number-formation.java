53c30e3a2251d84203b1be350a5b693230cb5590
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-10-20 11:34:46 +0530

    Added solution - LeetHub

--- Number Formation - GFG/number-formation.java
+++ Number Formation - GFG/number-formation.java
@@ -23,56 +23,35 @@ class GfG {
 // User Function template for JAVA
 
 class Solution {
+
     public int getSum(int X, int Y, int Z) {
-        int mod = 1000000007;
-    
-        long exactsum[][][] = new long[X + 1][Y+ 1][Z + 1];
-        long exactnum[][][] = new long[X + 1][Y + 1][Z + 1];
-        long ans = 0L;
-
-        exactnum[0][0][0] = 1L;
-
-        for (int i = 0; i <= X; ++i) {
-
-            for (int j = 0; j <= Y; ++j) {
-
-                for (int k = 0; k <= Z; ++k) {
-
-                    if (i > 0) {
-
-                        exactsum[i][j][k] += (exactsum[i - 1][j][k]*10 + 4*exactnum[i - 1][j][k]) %mod;
-                        exactnum[i][j][k] += exactnum[i - 1][j][k] % mod;
-
+        // Your code goes here
+        int mod=1000000007;
+        long[][][] exactSum=new long[X+1][Y+1][Z+1];
+        long[][][] num=new long[X+1][Y+1][Z+1];
+        num[0][0][0]=1L;
+        
+        long ans=0L;
+        for(int i=0;i<=X;i++){
+            for(int j=0;j<=Y;j++){
+                for(int k=0;k<=Z;k++){
+                    if(i>0){
+                        exactSum[i][j][k]+=(exactSum[i-1][j][k]*10 +4*num[i-1][j][k])%mod;
+                        num[i][j][k]+=num[i-1][j][k]%mod;
                     }
-
-                    if (j > 0) {
-
-                        exactsum[i][j][k] += (exactsum[i][j - 1][k] * 10 +5 * exactnum[i][j - 1][k]) %mod;
-
-                        exactnum[i][j][k] += exactnum[i][j - 1][k] % mod;
-
+                    if(j>0){
+                        exactSum[i][j][k]+=(exactSum[i][j-1][k]*10 +5*num[i][j-1][k])%mod;
+                        num[i][j][k]+=num[i][j-1][k]%mod;
                     }
-
-                    if (k > 0) {
-
-                        exactsum[i][j][k] += (exactsum[i][j][k - 1] * 10 +6 * exactnum[i][j][k - 1]) %mod;
-
-                        exactnum[i][j][k] += exactnum[i][j][k - 1] % mod;
-
+                    if(k>0){
+                        exactSum[i][j][k]+=(exactSum[i][j][k-1]*10 +6*num[i][j][k-1])%mod;
+                        num[i][j][k]+=num[i][j][k-1]%mod;
                     }
-
-                    ans += exactsum[i][j][k] % mod;
-
-                    ans %= mod;
-
+                    ans+=exactSum[i][j][k]%mod;
+                    ans%=mod;
                 }
-
             }
-
         }
-
         return (int)ans;
-
     }
-
 }
