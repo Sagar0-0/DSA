fe9e0d088a6f4e09a90626b52d2216838f08e54d
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-10-22 14:19:28 +0530

    Added solution - LeetHub

--- Filling Bucket - GFG/filling-bucket.java
+++ Filling Bucket - GFG/filling-bucket.java
@@ -25,13 +25,15 @@ class GFG {
 class Solution {
     static int fillingBucket(int N) {
         // code here
+        if(N==1)return 1;
+        int prev=2;
+        int secPrev=1;
         int mod=100000000;
-        int[]dp=new int[N+1];
-        dp[1]=1;
-        if(N>1)dp[2]=2;
-        for(int i=3;i<=N;i++){
-            dp[i]=(dp[i-1]%mod + dp[i-2]%mod)%mod;
+        for(int i=0;i<N-2;i++){
+            int currAns=(prev%mod+secPrev%mod)%mod;
+            secPrev=prev;
+            prev=currAns;
         }
-        return dp[N];
+        return prev;
     }
 };
