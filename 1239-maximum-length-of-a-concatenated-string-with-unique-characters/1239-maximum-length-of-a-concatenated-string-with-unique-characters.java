6425cf5dc8ef39f1c6b1c38f93b3fb4717b1ccc2
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-10-24 17:53:21 +0530

    Time: 4 ms (97.72%), Space: 42 MB (90.34%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,25 +1,32 @@
 class Solution {
-    int max =0;
+    public int solve(String strs[], int no, int a[], int index, int c) {
+        if(index==c)
+            return 0;
+        int x=no & a[index];
+        if(x==0)        
+            return Math.max(strs[index].length()+solve(strs,no^a[index],a,index+1,c),solve(strs,no,a,index+1,c));
+        else
+            return solve(strs,no,a,index+1,c);
+    }
     public int maxLength(List<String> arr) {
-        backTrack(arr,"",0);
-        return max;
-    }
-    
-    private void backTrack(List<String> arr, String current, int start){
-        if(max<current.length())max=current.length();
-        for(int i=start; i<arr.size();i++){
-            if(!isValid(current,arr.get(i)))continue;
-            backTrack(arr,current+arr.get(i),i+1);
+        int a[]=new int[arr.size()];int c=0;String strs[]=new String[a.length];
+        for(String s:arr) {
+            int x=0,fl=0;
+            for(char ch:s.toCharArray()) 
+            {
+                int z=x|(1<<(ch-96));
+                if(x==z)
+                {
+                    fl=1;break;                
+                }
+                x=z;
+            }
+            if(fl==0)
+            {
+                a[c]=x;
+                strs[c++]=s;
+            }
         }
-    }
-    
-    private boolean isValid(String currentString, String newString){
-        int[] array = new int[26];
-        for(int i=0;i<newString.length();i++){
-            if(++array[newString.charAt(i)-'a'] == 2) return false;  //to handle if the newString has duplicates then it's not valid
-            if(currentString.contains(newString.charAt(i)+"")) // check if the currentString contains each char from newString
-                return false;
-        }
-        return true;
+        return solve(strs,0,a,0,c);
     }
 }
