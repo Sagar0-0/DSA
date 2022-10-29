554cfb0c800aac1528fc64c417652a62f4ddba9d
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-10-29 18:41:10 +0530

    Time: 306 ms (10.05%), Space: 125 MB (40.64%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.java
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.java
@@ -1,18 +1,25 @@
 class Solution {
+    class Pair{
+        int plantTime;
+        int growTime;
+        Pair(int a,int b){
+            plantTime=a;
+            growTime=b;
+        }
+    }
     public int earliestFullBloom(int[] plantTime, int[] growTime) {
-        int n = growTime.length;
-        List<Integer> indices = new ArrayList<>();
-        for (int i = 0; i < n; ++i) {
-            indices.add(i);
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->b.growTime-a.growTime);
+        for(int i=0;i<plantTime.length;i++){
+            pq.add(new Pair(plantTime[i],growTime[i]));
         }
-        Collections.sort(indices, Comparator.comparingInt(i -> -growTime[i]));
-        int result = 0;
-        for (int i = 0, curPlantTime = 0; i < n; ++i) {
-            int idx = indices.get(i);
-            int time = curPlantTime + plantTime[idx] + growTime[idx];
-            result = Math.max(result, time);
-            curPlantTime += plantTime[idx];
+        int ans=0;
+        int currTime=0;
+        while(!pq.isEmpty()){
+            Pair p=pq.remove();
+            int time=currTime+p.plantTime + p.growTime;
+            ans=Math.max(ans,time);
+            currTime+=p.plantTime;
         }
-        return result;
+        return ans;
     }
 }
