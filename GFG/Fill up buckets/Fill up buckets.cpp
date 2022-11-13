e3268c1fed6c4353d10f64cd89058a947915da3d
Subasish Mula <74977100+SubasishMula2001@users.noreply.github.com>
2022-11-13 19:39:28 +0530

    Update Fill up buckets.cpp

--- GFG/Fill up buckets/Fill up buckets.cpp
+++ GFG/Fill up buckets/Fill up buckets.cpp
@@ -22,3 +22,23 @@ int b=a%1000000007;
 return b;
     }
 };
+
+
+//2nd Solution
+
+class Solution{
+public:
+int totalWays(int n, vector<int>capacity) {
+        long mod=1e9+7;
+       sort(capacity.begin(),capacity.end());
+       
+       long  ans=1;
+       
+       for(int i=0;i<n;i++)
+           ans=(ans*(capacity[i]-i))%mod;
+           
+        return ans;
+           
+       
+    }
+};
