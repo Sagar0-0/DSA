5a42aa4bff8a5ce8fe227de74c2d2036d0688108
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-11-16 10:10:50 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (20.66%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -1,25 +1,28 @@
-/** 
- * Forward declaration of guess API.
- * @param  num   your guess
- * @return 	     -1 if num is higher than the picked number
- *			      1 if num is lower than the picked number
- *               otherwise return 0
- * int guess(int num);
- */
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        int l=1;
-        int h=n;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            int res=guess(mid);
-            if(res==-1){
-                h=mid-1;
-            }else if(res==1){
-                l=mid+1;
-            }else{
-                return mid;
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid1 = low + (high - low) / 3;
+            int mid2 = high - (high - low) / 3;
+            int res1 = guess(mid1);
+            int res2 = guess(mid2);
+            if (res1 == 0)
+                return mid1;
+            if (res2 == 0)
+                return mid2;
+            else if (res1 < 0)
+                high = mid1 - 1;
+            else if (res2 > 0)
+                low = mid2 + 1;
+            else {
+                low = mid1 + 1;
+                high = mid2 - 1;
             }
         }
         return -1;
