8aaa4db06c8a192f7e16eacc80b60c1528a8fc5b
Gajjala Harsha <gvsshhrao1998@gmail.com>
2022-11-17 22:30:57 +0530

    Update Count of Subarrays.java

--- GFG/Count of Subarrays/Count of Subarrays.java
+++ GFG/Count of Subarrays/Count of Subarrays.java
@@ -17,3 +17,39 @@ class Solution {
         return ans;
     }
 }
+
+
+
+// Method 2
+
+class Solution {
+
+    long countSubarray(int arr[], int n, int k) {
+
+        // code here
+        int dy[]=new int[n];
+        int zero=-1;
+        for(int i=0;i<n;i++) {
+            if(arr[i]>k) dy[i]=n-i;
+            else {
+                dy[i]=zero;
+                zero-=1;
+            }
+        }
+        
+        long ans=0;
+        long curZero=0;
+        long zeros=0;
+        for(int i=0;i<n;i++) {
+            if(dy[i]>0)  {
+                curZero=zeros;
+                ans+=dy[i]+curZero;
+            }
+            else  {
+                ans+=curZero;
+                zeros=Math.abs(dy[i]);
+            }
+        }
+        return ans;
+    }
+}
