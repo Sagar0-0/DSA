e5e7ce57e6fe2757e487d7c955e39dd4c6abea4c
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-11-20 22:39:04 +0530

    Time: 3 ms (99.27%), Space: 44.5 MB (45.57%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,35 +1,23 @@
 class Solution {
+    int idx; // this index traverse the string in one pass, between different level of recursion
     public int calculate(String s) {
-        int sum = 0;
-        int sign = 1;
-        Stack<Integer>st = new Stack<>();
-        for(int i = 0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(Character.isDigit(ch)){
-                int val = 0;
-                 while(i < s.length() && Character.isDigit(s.charAt(i))){
-                     val = val * 10 + (s.charAt(i) - '0');
-                     i++;
-                 }
-             i--;
-             val = val * sign;
-             sign = 1;
-             sum += val;   
-            }
-            else if(ch == '('){
-              st.push(sum);
-              st.push(sign);
-                sum = 0;
-                sign = 1;
-            }
-            else if(ch == ')'){
-                sum *= st.pop();
-                sum += st.pop();
-            }
-            else if(ch == '-'){
-                sign*= -1;
+        idx = 0; // Initialization should be here
+        return calc(s);
+    }
+    
+    private int calc(String s) {
+        int res = 0, num = 0, sign = 1;
+        while (idx < s.length()) {
+            char c = s.charAt(idx++);
+            if (c >= '0' && c <= '9') num = num * 10 + c - '0';
+            else if (c == '(') num = calc(s); // ( is start of a new sub-problem, Let recursion solve the sub-problem
+            else if (c == ')') return res + sign * num;
+            else if (c == '+' || c == '-') { // only when we meet a new sign, we know a while number has been read
+                res += sign * num;
+                num = 0;
+                sign = c == '-' ? -1 : 1;
             }
         }
-        return sum;
+        return res + sign * num; // last number is not processed yet
     }
 }
