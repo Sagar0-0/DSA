094fb2fa8b40890265df76eb6eed4e6bc292a900
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-11-23 15:32:57 +0530

    Time: 2 ms (97.46%), Space: 45.5 MB (76.40%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,42 +1,30 @@
 class Solution {
+    public static boolean isCorrect(char[][] board,int x,int y){
+        for(int i=0;i<board.length;i++){
+            if(i!=y && board[x][i]==board[x][y]) return false;
+            if(i!=x && board[i][y]==board[x][y]) return false;
+        }
+        int row=(x/3)*3; 
+        int col=(y/3)*3;
+        for(int i=row;i<row+3;i++){
+            for(int j=col;j<col+3;j++){
+                if((i!=x && j!=y) && board[i][j]==board[x][y]) return false;
+            }
+        } 
+        return true;
+    }
+
     public boolean isValidSudoku(char[][] board) {
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
                 if(board[i][j]!='.'){
-                    if((checkRow(board,i,j) &&
-                    checkCol(board,i,j) &&
-                    checkBox(board,i,j))==false)return false;
+                    if(!isCorrect(board,i,j)){
+                        return false;
+                    }
                 }
             }
         }
         return true;
     }
-    boolean checkRow(char[][]board,int row,int col) {
-        for(int i=0;i<9;i++){
-            if(board[row][i]==board[row][col] && col!=i){
-                return false;
-            }
-        }
-        return true;
-    }
-    boolean checkCol(char[][]board,int row,int col) {
-        for(int i=0;i<9;i++){
-            if(board[i][col]==board[row][col] && row!=i){
-                return false;
-            }
-        }
-        return true;
-    }
-    boolean checkBox(char[][]board,int row,int col) {
-        int boxx=row/3;
-        int boxy=col/3;
-        int startx=3*boxx;
-        int starty=3*boxy;
-        for(int i=startx;i<startx+3;i++){
-            for(int j=starty;j<starty+3;j++){
-                if(board[i][j]==board[row][col] && (i!=row && j!=col))return false;
-            }
-        }
-        return true;
-    }
+    
 }
