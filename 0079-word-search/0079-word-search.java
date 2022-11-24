c7f44945a65a4fa507e7cb3a778c8ff1978c9ea5
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-11-24 11:35:44 +0530

    Time: 66 ms (98.81%), Space: 40.2 MB (93.46%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,34 +1,45 @@
 class Solution {
-	public boolean exist(char[][] board, String word) {
-		int rows = board.length, cols = board[0].length;                                // length of rows & columns 
-		if(word.length() == 0){ return true; }                                          // if "word" is empty, we will just return true. 
-		if(rows * cols < word.length()){ return false; }                                // if board size (rows*cols) < word length, => word has some character(s) not present in board. So, just return False. This is optimized way as we are avoiding doing DFS search in such test cases. 
 
-		// Traverse all chars of board
-		for(int r = 0; r < rows; r++){
-			for(int c = 0; c < cols; c++){
-				if(board[r][c] == word.charAt(0)){                                                // keep iterating until we don't find first character of "word"
-					boolean isFound = dfsBoard(board, new boolean[rows][cols], r, c, word, 0);    // call DFS or recursion
-					if(isFound){ return true; }
-				}
-			}
-		}
-		return false;                                                                             // if we traveresed all chars of board, it means we did not find all chars of word. so return false
-	}
-	// ----------------------------------------------------------------------------------------------------------------------------------------------- //
-	public boolean dfsBoard(char[][] board, boolean[][] visitedBoard, int row, int col, String word, int wordInd) {        			
-		if( (row < 0 || row >= board.length)   ||   (col < 0 || col >= board[0].length)  ||   board[row][col] != word.charAt(wordInd) ){
-			return false;                                                                             // return false, if:  a)  row or col goes out of bound.  b) char in board != char in word 
-		}
-		if(visitedBoard[row][col] ){ return false; }                                                  // this element already visited, so don't do DFS on this
-		if(wordInd == word.length() - 1){ return true; }
-		visitedBoard[row][col] = true;                                                                // mark it as visited now             
-			
-		if( dfsBoard(board, visitedBoard, row, col-1, word, wordInd + 1) ){ return true; }            // DFS on Left
-		if( dfsBoard(board, visitedBoard, row, col+1, word, wordInd + 1) ){ return true; }            // DFS on Right
-		if( dfsBoard(board, visitedBoard, row-1, col, word, wordInd + 1) ){ return true; }            // DFS on ABove
-		if( dfsBoard(board, visitedBoard, row+1, col, word, wordInd + 1) ){ return true; }            // DFS on Below
-		visitedBoard[row][col] = false;
-		return false;
-	}
+
+    public boolean exist(char[][] board, String word) {
+        char c = word.charAt(0);
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (c == board[i][j] && sub(board, word, i, j, 0, m, n, c))
+                    return true;
+
+        return false;
+    }
+
+    boolean exist(char[][] board, String word, int i, int j, int index, int m, int n) {
+        if (word.length() == index)
+            return true;
+        char c = word.charAt(index);
+
+        if (i > 0 && board[i - 1][j] == c
+                && sub(board, word, i - 1, j, index, m, n, c)) //top
+            return true;
+        if (i < m - 1 && board[i + 1][j] == c
+                && sub(board, word, i + 1, j, index, m, n, c)) //down
+            return true;
+        if (j > 0 && board[i][j - 1] == c && sub(board, word, i, j - 1, index, m, n, c)) //left
+            return true;
+        if (j < n - 1 && board[i][j + 1] == c) //right
+            return sub(board, word, i, j + 1, index, m, n, c);
+
+        return false;
+    }
+
+    boolean sub(char[][] board, String word, int i, int j,
+                int index, int m, int n, char c) {
+        board[i][j] = 0;
+        if (exist(board, word, i, j, index + 1, m, n))
+            return true;
+        else board[i][j] = c;
+        return false;
+    }
 }
+
+
