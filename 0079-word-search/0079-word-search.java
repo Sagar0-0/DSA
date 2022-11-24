8570901a39d0b5b009764fc6bb738c0fabd5e320
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-11-24 11:39:08 +0530

    Time: 2 ms (99.76%), Space: 42.6 MB (23.74%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,45 +1,64 @@
 class Solution {
-
-
+    int[][] dirs = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    boolean die;
+    
     public boolean exist(char[][] board, String word) {
-        char c = word.charAt(0);
-        int m = board.length;
-        int n = board[0].length;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (c == board[i][j] && sub(board, word, i, j, 0, m, n, c))
-                    return true;
-
+        int[] alphFreqForBoard = new int['z' - 'A' + 1]; 
+        int[] alphFreqForWord = new int['z' - 'A' + 1]; 
+        for (char cc : word.toCharArray()) {
+            alphFreqForWord[cc - 'A']++; 
+        }
+        
+        // count the alphabet occurance frequence in the board 
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                alphFreqForBoard[board[r][c] - 'A']++; 
+            }
+        }
+        
+        for (char cc : word.toCharArray()) {
+            if (alphFreqForBoard[cc - 'A'] < alphFreqForWord[cc - 'A']) {
+                return false; 
+            }
+        }
+        
+        if (alphFreqForBoard[word.charAt(0) - 'A'] > alphFreqForWord[word.charAt(word.length() - 1) - 'A']) {
+            char[] reverseWord = new char[word.length()]; 
+            for (int xx = 0, yy = word.length() - 1; yy >= 0; xx++, yy--) {
+                reverseWord[xx] = word.charAt(yy); 
+            }
+            word = new String(reverseWord); 
+        }
+        die = false;
+        
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                if(dfs(board, word, i, j, 0)) return true;
+            }
+        }
+        
         return false;
     }
-
-    boolean exist(char[][] board, String word, int i, int j, int index, int m, int n) {
-        if (word.length() == index)
-            return true;
-        char c = word.charAt(index);
-
-        if (i > 0 && board[i - 1][j] == c
-                && sub(board, word, i - 1, j, index, m, n, c)) //top
-            return true;
-        if (i < m - 1 && board[i + 1][j] == c
-                && sub(board, word, i + 1, j, index, m, n, c)) //down
-            return true;
-        if (j > 0 && board[i][j - 1] == c && sub(board, word, i, j - 1, index, m, n, c)) //left
-            return true;
-        if (j < n - 1 && board[i][j + 1] == c) //right
-            return sub(board, word, i, j + 1, index, m, n, c);
-
-        return false;
-    }
-
-    boolean sub(char[][] board, String word, int i, int j,
-                int index, int m, int n, char c) {
-        board[i][j] = 0;
-        if (exist(board, word, i, j, index + 1, m, n))
-            return true;
-        else board[i][j] = c;
-        return false;
+    
+    private boolean dfs(char[][] board, String word, int i, int j, int ci) {
+        if(die) return true;
+        else if(ci == word.length()) return die = true;
+        else if(i < 0 || i >= board.length || j < 0 || j >= board[0].length) return false;
+        
+        char c = board[i][j];
+        if(c != word.charAt(ci)) return false;
+        else if(c == '#') return false;
+        
+        boolean found = false;
+        board[i][j] = '#';
+        
+        for(int[] dir : dirs) {
+            found |= dfs(board, word, i+dir[0], j+dir[1], ci+1);
+            if(found) break;
+        }
+        
+        board[i][j] = c;
+        
+        return found;
     }
 }
-
-
