1556819b158e645c696327501f0008dfc1a0e9b0
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-11-26 00:01:48 +0530

    Time: 80 ms (74.12%), Space: 50 MB (84.38%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -4,39 +4,35 @@ class Solution {
         int MOD = 1000000007;
 
         Stack<Integer> stack = new Stack<>();
+        long sumOfMinimums = 0;
 
-        // make a dp array of the same size as the input array `arr`
-        int[] dp = new int[arr.length];
+        // building monotonically increasing stack
+        for (int i = 0; i <= arr.length; i++) {
 
-        // making a monotonic increasing stack
-        for (int i = 0; i < arr.length; i++) {
-            // pop the stack until it is empty or
-            // the top of the stack is greater than or equal to
-            // the current element
-            while (!stack.empty() && arr[stack.peek()] >= arr[i]) {
-                stack.pop();
+            // when i reaches the array length, it is an indication that
+            // all the elements have been processed, and the remaining
+            // elements in the stack should now be popped out.
+
+            while (!stack.empty() && (i == arr.length || arr[stack.peek()] >= arr[i])) {
+
+                // Notice the sign ">=", This ensures that no contribution
+                // is counted twice. rightBoundary takes equal or smaller 
+                // elements into account while leftBoundary takes only the
+                // strictly smaller elements into account
+
+                int mid = stack.pop();
+                int leftBoundary = stack.empty() ? -1 : stack.peek();
+                int rightBoundary = i;
+
+                // count of subarrays where mid is the minimum element
+                long count = (mid - leftBoundary) * (rightBoundary - mid) % MOD;
+
+                sumOfMinimums += (count * arr[mid]) % MOD;
+                sumOfMinimums %= MOD;
             }
-
-            // either the previousSmaller element exists
-            if (stack.size() > 0) {
-                int previousSmaller = stack.peek();
-                dp[i] = dp[previousSmaller] + (i - previousSmaller) * arr[i];
-            } else {
-                // or it doesn't exist, in this case the current element
-                // contributes with all subarrays ending at i
-                dp[i] = (i + 1) * arr[i];
-            }
-            // push the current index
             stack.push(i);
         }
 
-        // Add all elements of the dp to get the answer
-        long sumOfMinimums = 0;
-        for (int count : dp) {
-            sumOfMinimums += count;
-            sumOfMinimums %= MOD;
-        }
-
-        return (int) sumOfMinimums;
+        return (int) (sumOfMinimums);
     }
 }
