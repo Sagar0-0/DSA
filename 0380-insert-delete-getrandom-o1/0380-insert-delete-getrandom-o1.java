80ebfeb773a4db01b748b77cbaaabc2e3cb9032e
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-11-29 21:51:34 +0530

    Time: 35 ms (85.21%), Space: 87.8 MB (97.22%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -0,0 +1,49 @@
+class RandomizedSet {
+    List<Integer> list;
+    HashMap<Integer,Integer> map;
+    
+    public RandomizedSet() {
+        list=new ArrayList<>();
+        map=new HashMap<>();
+    }
+    
+    public boolean insert(int val) {
+        if(!map.containsKey(val)){
+            list.add(val);
+            map.put(val,list.size()-1);
+            return true;
+        }
+        return false;
+    }
+    
+    // Swap value to be removed with last value. Remove now the value at last 
+    // index & update map with new index and remove the value to be removed.
+    public boolean remove(int val) {
+        if(map.containsKey(val)){
+            int index=map.get(val);
+            swap(index);
+            list.remove(list.size()-1);
+            map.remove(val);
+            return true;
+        }
+        return false;
+    }
+    
+    // User defined.
+    private void swap(int index) {
+        int lastIndex=list.size()-1;
+        int lastVal=list.get(lastIndex);
+        
+        int temp=list.get(index);
+        list.set(index,lastVal);
+        list.set(lastIndex,temp);
+        
+        map.put(lastVal,index);
+    }
+    
+    public int getRandom() {
+        Random random=new Random();
+        int x=random.nextInt(list.size());
+        return list.get(x);
+    }
+}
