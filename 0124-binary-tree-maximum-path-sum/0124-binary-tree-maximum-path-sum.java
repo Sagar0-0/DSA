ae6ee10761d9b653e26ea59c39673393d0c6c9c9
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-12-11 17:29:41 +0530

    Time: 1 ms (99.63%), Space: 47.8 MB (43.82%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        maxSum = Integer.MIN_VALUE;
+        gainFromSubtree(root);
+        return maxSum;
+    }
+
+    private int maxSum;
+
+    // post order traversal of subtree rooted at `root`
+    private int gainFromSubtree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        // add the path sum from left subtree. Note that if the path
+        // sum is negative, we can ignore it, or count it as 0.
+        // This is the reason we use `Math.max` here.
+        int gainFromLeft = Math.max(gainFromSubtree(root.left), 0);
+
+        // add the path sum from right subtree. 0 if negative
+        int gainFromRight = Math.max(gainFromSubtree(root.right), 0);
+
+        // if left or right path sum are negative, they are counted
+        // as 0, so this statement takes care of all four scenarios
+        maxSum = Math.max(maxSum, gainFromLeft + gainFromRight + root.val);
+
+        // return the max sum for a path starting at the root of subtree
+        return Math.max(gainFromLeft + root.val, gainFromRight + root.val);
+    }
+}
