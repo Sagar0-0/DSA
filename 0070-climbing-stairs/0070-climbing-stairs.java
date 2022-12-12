3796985a479399f4bd279f1f4439bc76d3cb5390
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-12-12 13:13:23 +0530

    Time: 0 ms (100.00%), Space: 41 MB (38.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,12 @@
 class Solution {
-    int []dp;
     public int climbStairs(int n) {
-        dp=new int[n+1];
-        return dfs(n);
-    }
-    int dfs(int n){
-        if(n<=2)return n;
-        if(dp[n]!=0)return dp[n];
-        return dp[n]=dfs(n-1)+dfs(n-2);
+        if (n < 3) return n;
+        int oneStep = 1, twoStep = 2;
+        for (int i = 3; i <= n; i++) {
+            int count = oneStep + twoStep;
+            oneStep = twoStep;
+            twoStep = count;
+        }
+        return twoStep;
     }
 }
