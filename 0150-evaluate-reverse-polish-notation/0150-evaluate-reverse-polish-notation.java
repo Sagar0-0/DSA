8f9b91068226ec2c1a27fd935f6427e127df94d2
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-12-17 22:43:08 +0530

    Time: 2 ms (99.83%), Space: 44.3 MB (32.83%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,24 +1,38 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> st=new Stack<>();
-        for(String s:tokens){
-            if(s.length()>1 || Character.isDigit(s.charAt(0))){
-                st.push(Integer.parseInt(s));
-            }else{
-                int a=st.pop();
-                int b=st.pop();
-                char ch=s.charAt(0);
-                if(ch=='+'){
-                    st.push(a+b);
-                }else if(ch=='-'){
-                    st.push(b-a);
-                }else if(ch=='*'){
-                    st.push(a*b);
-                }else{
-                    st.push(b/a);
-                }
+                  int[] nxtPos = new int[1];
+
+          return eval(tokens, tokens.length - 1, nxtPos);
+    }
+
+    private int eval(String[] tokens, int pos, int[] nxtPos) {
+
+        char ac = tokens[pos].charAt(0);
+
+        if (tokens[pos].length() == 1 && (ac == '+' || ac == '-' || ac == '*' || ac == '/')) {
+
+            // get operands
+            int a = eval(tokens, pos - 1, nxtPos);
+            int b = eval(tokens, nxtPos[0], nxtPos);
+
+            // perform operation
+            if (ac == '+') {
+                
+                return b + a;
+            } else if (ac == '-') {
+
+                return b - a;
+            } else if (ac == '*') {
+
+                return b * a;
+            } else {
+
+                return b / a;
             }
+        } else {
+
+            nxtPos[0] = pos - 1;
+            return Integer.parseInt(tokens[pos]);
         }
-        return st.pop();
     }
 }
