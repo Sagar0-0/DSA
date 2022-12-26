4e1fac7c19b50eb2f6e4a1e6b0212fddcf4527ae
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2022-12-26 09:28:22 +0530

    Time: 2 ms (92.42%), Space: 43.1 MB (78.17%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,33 +1,12 @@
 class Solution {
-    byte[][]dp;
     public boolean canJump(int[] nums) {
-        int max=0;
-        for(int i:nums)max=Math.max(max,i);
-        dp=new byte[nums.length][max+1];
-        return dfs(0,nums);
-    }
-    boolean dfs(int i,int nums[]){
-        if(i==nums.length-1)return true;
-        if(nums[i]==0)return false;
-        int num=nums[i];
-        if(dp[i][num]>0){
-            if(dp[i][num]==1)return false;
-            else return true;
+        int maxReachableDistance=nums[0];
+        int i=1;
+        while(maxReachableDistance>=i && i<nums.length){
+            maxReachableDistance=Math.max(maxReachableDistance,nums[i]+i);
+            i++;
         }
-        while(num>0){
-            if(i+num<nums.length){
-                if(dfs(i+num,nums)){
-                    dp[i][nums[i]]=2;
-                    dp[i][num]=2;
-                    return true;
-                }else{
-                    dp[i][nums[i]]=1;
-                    dp[i][num]=1;
-                }
-            }
-            num--;
-        }
-        return false;
+        return maxReachableDistance>=nums.length-1;
+        
     }
-    
 }
