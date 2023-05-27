11146461dc38c723125ff34c3a162cd5acd6e012
raghav-1998 <raghav.vasdeva@gmail.com>
2023-05-28 00:45:22 +0530

    Adding Python Solution

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        d = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+        res = ''
+        for k in d:
+            while num >= k:
+                res += d[k]
+                num -= k
+        return res
