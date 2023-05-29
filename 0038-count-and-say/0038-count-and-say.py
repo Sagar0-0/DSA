4f816a6d4baaac37064851458b71f36fe33507ae
raghav-1998 <raghav.vasdeva@gmail.com>
2023-05-30 00:17:29 +0530

    Adding python solution of count & say problem

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,7 @@
+import re
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = '1'
+        for _ in range(n - 1):
+            s = re.sub(r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1), s)
+        return s
